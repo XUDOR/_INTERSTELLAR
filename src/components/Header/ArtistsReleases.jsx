d7b20eb7846181e5e20a5cf69a662234aa326883
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-20 22:53:29 -0700

    added EPK block

--- src/components/Header/ArtistsReleases.jsx
+++ src/components/Header/ArtistsReleases.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ArtistsReleases.css';
+import EPK from './EPK';
 
 const ArtistsReleases = () => {
   return (
     <div className="artists-releases">
       Artists
+      <EPK />
     </div>
   );
 };
