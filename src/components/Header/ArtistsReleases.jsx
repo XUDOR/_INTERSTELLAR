760d4106dc3b6b61715bc251439db38d104dd29d
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-20 21:04:50 -0700

    artist container

---
+++ src/components/Header/ArtistsReleases.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ArtistsReleases.css';
+
+const ArtistsReleases = () => {
+  return (
+    <div className="artists-releases">
+      Artists
+    </div>
+  );
+};
+
+export default ArtistsReleases;
