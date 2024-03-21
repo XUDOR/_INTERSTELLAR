31633bf5e6b8bcd5ade5e1c6254133426f537ace
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-21 14:28:49 -0700

    MOD: from "content" to <Album> jsx object

--- src/components/Gallery/Gallery_Display.jsx
+++ src/components/Gallery/Gallery_Display.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import Gallery from './Gallery';
 import GalleryItem from './GalleryItem';
 import './Gallery.css';
+import Album from '../Content/Album/Album';
 
 function Gallery_Display() {
   return (
     <div className="Gallery_Display">
       <Gallery>
         <GalleryItem label="1">
-          Content for Artist 1
+          <Album></Album>
         </GalleryItem>
         <GalleryItem label="2">
-          Content for Album 2
+          Content 2
         </GalleryItem>
         <GalleryItem label="3">
-          Content for Artist/Album 3
+          Content 3
         </GalleryItem>      
       </Gallery>
     </div>
