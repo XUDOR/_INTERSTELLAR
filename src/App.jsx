1e78688f350be39069471bc7c3d323e3b08c58f8
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-20 22:10:45 -0700

    adding Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header/Header'; // Assuming Header is properly set up
 import GalleryDisplay from './components/Gallery/Gallery_Display';
 // Assuming you'll create and import Footer later
-// import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer'
 import './App.css'; // Make sure to have global styles that apply to the entire app
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         {/* Place other components here as your app grows */}
       </main>
       {/* Footer will eventually go here */}
-      {/* <Footer /> */}
+       <Footer /> 
     </div>
   );
 }
