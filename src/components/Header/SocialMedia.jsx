88cc7a34991de178fe62a8b6db00784e7fcc29fe
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-20 21:07:48 -0700

    container -- basic start

---
+++ src/components/Header/SocialMedia.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SocialMedia.css';
+
+const SocialMedia = () => {
+  return (
+    <div className="social-media">
+      Social Media
+    </div>
+  );
+};
+
+export default SocialMedia;
