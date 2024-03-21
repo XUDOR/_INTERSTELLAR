f217b6202f0f9a891422ef77d91ed172fc174009
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-20 21:05:32 -0700

    start container for Brand/ has name --will have svg

---
+++ src/components/Header/Brand.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Brand.css';
+
+const Brand = () => {
+  return (
+    <div className="brand">
+      Interstellar Packages
+    </div>
+  );
+};
+
+export default Brand;
