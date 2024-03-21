aab460541d00f73f290b9799fe21d1cb3f718155
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-21 13:01:23 -0700

    fix

--- src/components/Gallery/GalleryItem.jsx
+++ src/components/Gallery/GalleryItem.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const GalleryItem = ({ children }) => {
-  return <div className="container">{children}</div>;
+  return <div className="container">{children}
+  </div>;
 };
 
 export default GalleryItem;
