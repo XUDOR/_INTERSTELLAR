6c8f00fff44b590904605a416eee70c4fa15fada
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-20 22:52:58 -0700

    added shop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,15 @@
 import React from 'react';
 import Player from './Player';
 import Radio from './Radio';
+import Shop from './Shop';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="Footer">
       <Player />
-     
       <Radio />
+      <Shop />
     </footer>
   );
 };
