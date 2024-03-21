494ac4909bdf8c8b90d32b3a420945135b1c961e
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-20 22:13:30 -0700

    rename from tabPanel to GalleryItem

--- src/components/Gallery/GalleryItem.jsx
+++ src/components/Gallery/GalleryItem.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const TabPanel = ({ children }) => {
+const GalleryItem = ({ children }) => {
   return <div className="container">{children}</div>;
 };
 
-export default TabPanel;
+export default GalleryItem;
 
