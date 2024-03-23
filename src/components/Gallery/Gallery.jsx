28561b9f10111d9fa5f1cdaa8ef1cdd8068a9f28
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-23 13:17:33 -0700

    striping code from gallery to TabList component

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,32 +1,20 @@
+// Gallery.jsx
 import React, { useState } from 'react';
-import './Gallery.css';
-
+import TabList from '../MainSection/TabList'; // Assuming it's in the same directory
+//import './Gallery.css';
 
 const Gallery = ({ children }) => {
   const [activeTab, setActiveTab] = useState(children[0].props.label);
 
-  const handleClick = (e, newActiveTab) => {
-    e.preventDefault();
-    setActiveTab(newActiveTab);
+  const handleTabClick = (label) => {
+    setActiveTab(label);
   };
 
-  return(
+  return (
     <div>
-      <ul className="tab-list">
-        {children.map((tab) => {
-          const label = tab.props.label;
-        
-          return (
-            <li 
-            className={label === activeTab ? 'tab-list-item active' : 'tab-list-item'}
-            key={label}
-            onClick={(e) => handleClick(e, label)}
-            >
-              {label}
-            </li>
-          );
-        })}
-      </ul>
+      <TabList activeTab={activeTab} onTabClick={handleTabClick}>
+        {children}
+      </TabList>
       <div className="tab-content">
         {children.map((one) => {
           if (one.props.label !== activeTab) return undefined;
