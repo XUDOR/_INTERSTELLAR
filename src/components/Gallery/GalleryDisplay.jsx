1ed58b2a8639f1c2ed9a48dcb7cce778ec112270
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-23 14:41:04 -0700

    state and props for TabList

--- src/components/Gallery/GalleryDisplay.jsx
+++ src/components/Gallery/GalleryDisplay.jsx
@@ -1,30 +1,34 @@
+// GalleryDisplay.jsx
 import React from 'react';
 import Gallery from './Gallery';
 import GalleryItem from './GalleryItem';
 import Album from '../Content/Album/Album';
 import AlbumTitle from '../Content/Album/AlbumTitle';
-import '../Content/Album/AlbumTitle.css';
-import './GalleryDisplay.css'
-import './GalleryItem.css'
+import './GalleryDisplay.css';
+import './GalleryItem.css';
 
+function GalleryDisplay({ activeTab, setActiveTab }) {
+  // Function to handle tab selection change
+  const handleTabClick = (label) => {
+    setActiveTab(label);
+  };
 
-function Gallery_Display() {
   return (
-    <div className="Gallery_Display">
-      <Gallery>
+    <div className="GalleryDisplay">
+      <Gallery activeTab={activeTab} onTabClick={handleTabClick}>
         <GalleryItem label="1">
-          <AlbumTitle></AlbumTitle>
-          <Album></Album>
+          <AlbumTitle />
+          <Album />
         </GalleryItem>
         <GalleryItem label="2">
-          #2
+          Content 2
         </GalleryItem>
         <GalleryItem label="3">
-          #3
-        </GalleryItem>      
+          Content 3
+        </GalleryItem>
       </Gallery>
     </div>
   );
 }
 
-export default Gallery_Display;
+export default GalleryDisplay;
