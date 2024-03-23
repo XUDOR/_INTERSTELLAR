c396a9eae696294dd344897569de9f93b26af985
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-23 14:39:03 -0700

    handling state for tablist

--- src/components/MainSection/Main.jsx
+++ src/components/MainSection/Main.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+// Main.jsx
+import React, { useState } from 'react';
 import GalleryDisplay from '../Gallery/GalleryDisplay';
-import Current from '../Current/Current';
-import './Main.css'
+import TabList from './TabList'; // Adjust the path as necessary
+import Current from '../Current/Current'; // Corrected path
+import './Main.css';
 
 function Main() {
+  // The state for the active tab is managed here
+  const [activeTab, setActiveTab] = useState('1');
+
+  // This method is used to change the active tab
+  const handleTabClick = (label) => {
+    setActiveTab(label);
+  };
+
+  // Labels for the tabs
+  const tabLabels = ['1', '2', '3'];
+
   return (
-      <div className='Main'>
-          <GalleryDisplay></GalleryDisplay>
-          <Current></Current>
-      </div>
+    <div className='Main'>
+      <TabList activeTab={activeTab} onTabClick={handleTabClick} labels={tabLabels} />
+      <GalleryDisplay activeTab={activeTab} onSetActiveTab={setActiveTab} />
+      <Current />
+    </div>
   );
 }
 
