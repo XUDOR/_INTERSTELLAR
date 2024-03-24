10834ad19703d278cf7e6f6bf1fcfccd8cacc8cc
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-23 21:58:22 -0700

    Redundant import removed

--- src/components/Content/Album/Album.jsx
+++ src/components/Content/Album/Album.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Cover from './Cover';
-import SongList from './SongList';
-import InfoContainer from './InfoContainer';
 import './Album.css';
 import coverImage from '../../../images/CHARLOTTA_cover.png';
 
