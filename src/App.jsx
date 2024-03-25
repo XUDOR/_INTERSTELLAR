07e2287002634870fcfebe86d5cd23705a1cdf35
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 19:07:40 -0700

    router implimentation \ basic start of routes for shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-// App.jsx
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import React from 'react';
 import Header from './components/Header/Header'; 
 import Main from './components/MainSection/Main';
-import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer';
+import ShopPage from './components/Shop-Cart/ShopPage';
 import './App.css'; 
 
-function App() {
+const AppWrapper = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+const App = () => {
+  let location = useLocation(); // This hook must be used within a component that is a child of <Router>
+
   return (
     <div className="app">
-      <Header />
+      {location.pathname !== '/shop' && <Header />}
       <main>
-        <Main />   
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/shop" element={<ShopPage minimalHeader={true} />} />
+        </Routes>
       </main>
-       <Footer /> 
+      {location.pathname !== '/shop' && <Footer />}
     </div>
   );
 }
 
-export default App;
+export default AppWrapper;
