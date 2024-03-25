9d43e0c474e81be9a06e9b6f7f5ca9bf6525b4ac
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 23:51:40 -0700

    refactor for header debug

--- src/components/ShopCart/ShopPage.jsx
+++ src/components/ShopCart/ShopPage.jsx
@@ -1,12 +1,12 @@
 // ShopPage.jsx
 import React from 'react';
-import Header from '../Header/Header';
+
 import './ShopPage.css'; // Assuming you have custom styles for ShopPage
 
 const ShopPage = () => {
   return (
     <>
-      <Header className="shop-header" />
+      {/*<Header className="shop-header" />*/}
       <div className="shopContent"> {/* Add this wrapper */}
         <div className='shopTitle'>Shop</div>
       </div>
