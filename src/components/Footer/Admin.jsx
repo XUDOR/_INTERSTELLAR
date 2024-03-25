47c6e99f3797de7ce9022e103e5ccc511593bb74
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 19:10:09 -0700

    div-simple start for UI to pages

---
+++ src/components/Footer/Admin.jsx
@@ -0,0 +1,13 @@
+//Footer/Admin.jsx
+import React from 'react';
+import './Admin.css';
+
+const Admin = () => {
+  return (
+    <div className="Admin">
+      Admin
+    </div>
+  );
+};
+
+export default Admin;
---
+++ src/components/Footer/User.jsx
@@ -0,0 +1,13 @@
+//Footer/User.jsx
+import React from 'react';
+import './User.css';
+
+const User = () => {
+  return (
+    <div className="User">
+      User
+    </div>
+  );
+};
+
+export default User;
