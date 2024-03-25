2f449387dc5cf1dc9463a4801df6b517f4efc2ad
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 19:12:49 -0700

    update for onClick & useNavigate to shop router route and -> page

--- src/components/Footer/Shop.jsx
+++ src/components/Footer/Shop.jsx
@@ -1,11 +1,18 @@
-// Shop.jsx
-
+// Import React if you're using any React features like hooks
 import React from 'react';
+// Import the CSS file for the Shop component
 import './Shop.css';
+import { useNavigate } from 'react-router-dom';
 
-const Shop = ({ onShopClick }) => {
+const Shop = () => {
+  let navigate = useNavigate();
+
+  const handleShopClick = () => {
+    navigate('/shop');
+  };
+
   return (
-    <div className="Shop" onClick={onShopClick}>
+    <div className="Shop" onClick={handleShopClick}>
       Shop
     </div>
   );
