fc4e1e981a159f30c50f15cfbf68b483de03c24b
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 19:10:35 -0700

    update to container for divs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import Player from './Player';
 import Radio from './Radio';
 import Shop from './Shop';
+import Nav from './Nav';
+import Admin from './Admin';
+import User from './User';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="Footer">
+      <Admin />
+      <User />
+      <Nav />
       <Player />
       <Radio />
       <Shop />
