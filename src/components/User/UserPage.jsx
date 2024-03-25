fbdcd1bb23e6410f90215718a4853d91404588c7
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 23:55:57 -0700

    page-start

---
+++ src/components/User/UserPage.jsx
@@ -0,0 +1,15 @@
+// UserPage.jsx
+import React from 'react';
+import './UserPage.css'; 
+
+const UserPage = () => {
+  return (
+    <>
+      <div className="userContent"> {/* Add this wrapper */}
+        <div className='userTitle'>_User</div>
+      </div>
+    </>
+  );
+};
+
+export default UserPage;
