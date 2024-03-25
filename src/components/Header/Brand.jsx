7ab1bc18390bb2440732d21baa9ff21916be9669
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 19:14:14 -0700

    {link} back to home / main

--- src/components/Header/Brand.jsx
+++ src/components/Header/Brand.jsx
@@ -1,13 +1,15 @@
-// Header/brand.jsx
-
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link component
 import './Brand.css';
 import LogoSVG from '../../images/IP_TAG24.svg';
 
 const Brand = () => {
   return (
     <div className="brand">
-      <img src ={LogoSVG} alt="LOGO" />
+      {/* Wrap your img tag in a Link tag */}
+      <Link to="/">
+        <img src={LogoSVG} alt="Logo" />
+      </Link>
     </div>
   );
 };
