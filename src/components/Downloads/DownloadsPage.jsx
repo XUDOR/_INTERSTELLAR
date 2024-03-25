4b1acf144f93ef57ad17a56f679639867bda5e8e
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-25 16:34:04 -0700

    basic page -start

---
+++ src/components/Downloads/DownloadsPage.jsx
@@ -0,0 +1,17 @@
+// DownloadsPage.jsx
+import React from 'react';
+
+import './DownloadsPage.css'; 
+
+const DownloadsPage = () => {
+  return (
+    <>
+     
+      <div className="DownloadsContent"> {/* Add this wrapper */}
+        <div className='DownloadsTitle'>Downloads</div>
+      </div>
+    </>
+  );
+};
+
+export default DownloadsPage;
