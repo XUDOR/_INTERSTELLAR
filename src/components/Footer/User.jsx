39d6e27bdb43dbb2e8861d22ef5804f8da7e9a38
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 23:52:18 -0700

    nav onCLick for route & page

--- src/components/Footer/User.jsx
+++ src/components/Footer/User.jsx
@@ -1,10 +1,17 @@
-//Footer/User.jsx
+// Import React if you're using any React features like hooks
 import React from 'react';
 import './User.css';
+import { useNavigate } from 'react-router-dom';
 
 const User = () => {
+  let navigate = useNavigate();
+
+  const handleShopClick = () => {
+    navigate('/user');
+  };
+
   return (
-    <div className="User">
+    <div className="User" onClick={handleShopClick}>
       User
     </div>
   );
