f505df1fc7c4d84a2209e5a0b18e6c6e8e8f2ca5
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-24 23:53:49 -0700

    custom hook for conditional isMinimal? and path.page array

---
+++ src/hooks/useHeaderStyle.js
@@ -0,0 +1,13 @@
+// useHeaderStyle.js
+import { useLocation } from 'react-router-dom';
+
+export function useHeaderStyle() {
+  const location = useLocation();
+  const minimalHeaderPaths = ['/shop', '/admin', '/user'];
+  const isMinimal = minimalHeaderPaths.includes(location.pathname);
+
+  return {
+    isMinimal: isMinimal, // A boolean that tells you if the header should be minimal
+    headerClassName: isMinimal ? 'minimal-header' : '', // The class name for the header
+  };
+}
