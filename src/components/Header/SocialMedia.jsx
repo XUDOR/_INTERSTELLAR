2f4d40abc68e1e46c50777c141b3abc2f6a3c16f
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-25 17:29:49 -0700

    link added and import Router - link added

--- src/components/Header/SocialMedia.jsx
+++ src/components/Header/SocialMedia.jsx
@@ -1,12 +1,11 @@
-// Header/SocialMedia.jsx
-
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SocialMedia.css';
 
 const SocialMedia = () => {
   return (
     <div className="social-media">
-      Social Media | Contact
+      Social Media | <Link to="/contact" className="contact-link">Contact</Link>
     </div>
   );
 };
