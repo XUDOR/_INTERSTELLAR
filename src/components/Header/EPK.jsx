5eee6b759f1d6135417408c9e75c2255006514c8
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-25 17:28:31 -0700

    nav link in socialMedia container

--- src/components/Header/EPK.jsx
+++ src/components/Header/EPK.jsx
@@ -1,11 +1,17 @@
-//Header/EPK.jsx
-
+// Import React if you're using any React features like hooks
 import React from 'react';
 import './EPK.css';
+import { useNavigate } from 'react-router-dom';
 
 const EPK = () => {
+  let navigate = useNavigate();
+
+  const handleShopClick = () => {
+    navigate('/epk');
+  };
+
   return (
-    <div className="EPK">
+    <div className="EPK" onClick={handleShopClick}>
       EPK
     </div>
   );
