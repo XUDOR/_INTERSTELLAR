77325be30e5ef9fe1669b523f0a817dad60b2304
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-26 10:34:00 -0700

    cover link revert from url to static / hook commetd out

--- src/components/Content/Album/Album.jsx
+++ src/components/Content/Album/Album.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Cover from './Cover';
 import './Album.css';
+
+// Potentially use this hook for dynamically fetching an authenticated image
+// import useAuthenticatedImage from '../../../hooks/useAuthenticatedImage';
 import coverImage from '../../../images/CHARLOTTA_cover.png';
 
 
-
 function Album() {
-  const coverImageUrl = coverImage;
+  // Uncomment and use the line below for dynamic image fetching
+  // const coverImageUrl = useAuthenticatedImage();
+  const coverImageUrl = coverImage; // Use the imported image for now
   return (
     <div className='Album'>
-      <Cover imageUrl={coverImageUrl}/>
+      {/* Pass the image URL to the Cover component */}
+      <Cover imageUrl={coverImageUrl} />
     </div>
   );
-
 }
-
 
 export default Album;
