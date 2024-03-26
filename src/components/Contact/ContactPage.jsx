9ba8aa26ba824e0574d8d7f5a390b817eb31d814
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-25 17:27:15 -0700

    basic page -start

---
+++ src/components/Contact/ContactPage.jsx
@@ -0,0 +1,16 @@
+// AdminPage.jsx
+import React from 'react';
+import './ContactPage.css'; 
+
+const ContactPage = () => {
+  return (
+    <>
+     
+      <div className="ContactContent"> {/* Add this wrapper */}
+        <div className='ContactTitle'>_Contact</div>
+      </div>
+    </>
+  );
+};
+
+export default ContactPage;
