d5eb4a0c1acc2bfb4f3f4a0f7b1eaf0ec80f6087
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-26 10:38:34 -0700

    comments for  props in cover

--- src/components/Content/Album/Cover.jsx
+++ src/components/Content/Album/Cover.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Cover.css';
 
+// Assuming Cover accepts imageUrl as a prop
 function Cover({ imageUrl }) {
   return (
     <div className='Cover'>
+      {/* Render the image using the imageUrl prop */}
       <img src={imageUrl} alt="Album Cover" />
     </div>
   );
