1a787b0044e3195f87b806df0e7cf1bb829c4dec
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-25 17:28:05 -0700

    basic page-start

---
+++ src/components/EPKPage/EPKPage.jsx
@@ -0,0 +1,17 @@
+// DownloadsPage.jsx
+import React from 'react';
+
+import './EPKPage.css'; 
+
+const EPKPage = () => {
+  return (
+    <>
+     
+      <div className="EPKContent"> {/* Add this wrapper */}
+        <div className='EPKTitle'>_EPK</div>
+      </div>
+    </>
+  );
+};
+
+export default EPKPage;
