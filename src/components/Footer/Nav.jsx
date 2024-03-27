0797e1a7316ed2c79df6957dba8a79b7ddf4d74d
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-26 22:52:03 -0700

    starting to build out nav

--- src/components/Footer/Nav.jsx
+++ src/components/Footer/Nav.jsx
@@ -1,8 +1,14 @@
 //Footer/Nav.js
 import React from 'react';
+//import { Link } from 'react-router-dom'; // Import Link component
 import './Nav.css';
 
 const Nav = () => {
+  //let navigate = useNavigate();
+
+  //const handleShopClick = () => {
+    //navigate('/home');
+  //};
   return (
     <div className="Nav">
       Nav
