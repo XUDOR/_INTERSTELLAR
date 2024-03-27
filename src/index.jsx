a8306b552e6876fe3485ae51bc89ed1d2f1f2924
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-26 18:04:27 -0700

    moving App out of index for AppWrapper

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,18 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppWrapper from './AppWrapper';
 import './index.css';
-import App from './App'; // This now points to the new App.jsx file
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>
 );
+
+console.log('ReactDOM rendered');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
