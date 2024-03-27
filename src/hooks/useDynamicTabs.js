cd9640c306aa2af72348fac6bea978d7ac6f9b11
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-26 18:07:47 -0700

    custom hook for dynamic tabs

---
+++ src/hooks/useDynamicTabs.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+// Revised custom hook to manage dynamic tab labels, returning an object
+function useDynamicTabs(initialLabels) {
+  const [tabLabels, setTabLabels] = useState(initialLabels);
+
+  // Function to update tab labels
+  const updateTabLabels = (newLabels) => {
+    setTabLabels(newLabels);
+  };
+
+  // Return as an object to destructure conveniently
+  return { tabLabels, updateTabLabels };
+}
+
+export default useDynamicTabs;
