1228f03a54cf2ed8691ec5b1cf5f78ff67414d79
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-27 15:50:18 -0700

    beginning to impliment cart.jsx

--- src/components/ShopCart/ShopPage.jsx
+++ src/components/ShopCart/ShopPage.jsx
@@ -1,13 +1,16 @@
 // ShopPage.jsx
 import React from 'react';
-
+import Cart from '../Cart/Cart';
 import './ShopPage.css'; // Assuming you have custom styles for ShopPage
+import '../Cart/Cart.css'
 
 const ShopPage = () => {
   return (
     <>
       <div className="shopContent"> {/* Add this wrapper */}
         <div className='shopTitle'>Shop</div>
+      {/*<Cart />*/}
+
       </div>
     </>
   );
