90083196ca0f1180c5ec9157e4aa9b4930a54e31
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-28 21:12:52 -0700

    album seed 1

---
+++ server/DB/seeds/album_seed1.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO albums 
+(id, name, cover_url, production_date, release_date, artist_id, description, tracks) 
+VALUES 
+(1,
+  'Album Name', 
+ 'http://example.com/cover.jpg', 
+ '2005-05-01', 
+ '2010-10-03', 
+ 1, 
+ 'The debut ambient electronic album by Roderick Shoolbraid, 
+ written in a span from 2003 to 2005. Released in 2010.  
+ A soundtrack for a dream, and a film that never was.', 
+ 8);
