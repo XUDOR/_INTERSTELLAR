49159f3b6c9f2eb3ced92dcf0f97fb0ccc18ee76
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-28 21:13:14 -0700

    song seed 1

---
+++ server/DB/seeds/song_seed1.sql
@@ -0,0 +1,10 @@
+INSERT INTO songs (id, name, audio_url, artist_id, album_id, track_id) 
+VALUES 
+('0101', 'Charlotta', 'http://example.com/audio/song1.mp3', 1, 1, 1),
+('0102', 'Garden Mix', 'http://example.com/audio/song2.mp3', 1, 1, 2),
+('0103', 'New Ornithologies', 'http://example.com/audio/song3.mp3', 1, 1, 3),
+('0104', 'Old Flowers', 'http://example.com/audio/song4.mp3', 1, 1, 4),
+('0105', 'Windows', 'http://example.com/audio/song5.mp3', 1, 1, 5),
+('0106', 'Beautiful Waters', 'http://example.com/audio/song6.mp3', 1, 1, 6),
+('0107', 'Yellow Transports', 'http://example.com/audio/song7.mp3', 1, 1, 7),
+('0108', 'Perfumed Letter', 'http://example.com/audio/song8.mp3', 1, 1, 8);
