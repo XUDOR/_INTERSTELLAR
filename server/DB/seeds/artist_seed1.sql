1c14f1460298e96273e89e6a0d4f1242b82eb58a
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-28 21:13:03 -0700

    artist seed 1

---
+++ server/DB/seeds/artist_seed1.sql
@@ -0,0 +1,7 @@
+INSERT INTO artists (id, name, description) 
+VALUES 
+(1, 'Roderick Shoolbraid', '
+Multi-disciplinary Artist and experimental musician with 35 years of experience in sound design. 
+Currently residing in Sidney, BC, known for innovative musical compositions and soundscapes that 
+blend various genres and techniques.'
+);
