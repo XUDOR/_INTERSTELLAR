a41b4e2b1964c72a2d263c7ae9cbed40748b14d5
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-28 21:27:25 -0700

    basic express server w/ test

--- server/server.js
+++ server/server.js
@@ -1,19 +1,38 @@
+require('dotenv').config(); // Make sure to require dotenv at the top
 const express = require('express');
-const cors = require('cors'); // Import CORS module
+const cors = require('cors');
+const { Pool } = require('pg');
+
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors()); // Use CORS middleware here, before defining any routes
+// Setup your PostgreSQL pool connection here
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
-// Import the imageRoutes module
-//const imageRoutes = require('./routes/imageRoutes'); ------> commented to toogle to static route for hook
-const imageRoutes = require('./routes/staticRoutes');
+app.use(express.json()); // This line is needed to parse JSON bodies
+app.use(cors());
 
+// Here, you can include your other routes and middleware
+// ...
 
-// Use the routes with the '/api' prefix
-app.use('/api', imageRoutes);
+// Testing database connection
+app.get('/test-db', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT NOW()');
+    res.json(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
 
-// Basic route for testing
+// Your basic Hello World route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
