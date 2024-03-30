8d11490c4f04007c02e9a4d22c4ba45af8b57e65
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-30 11:55:10 -0700

    error handling middleware

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ require('dotenv').config(); // Make sure to require dotenv at the top
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes'); // Import centralized routes
-// Removed direct albumRoutes import since it's now part of centralized routing
 const pool = require('./db'); // Assuming db.js is set up for global use
 
 const app = express();
@@ -13,6 +12,12 @@ app.use(cors());
 
 // Use centralized routes with '/api' as the base path
 app.use('/api', routes);
+
+// Error handling middleware should be the last piece of middleware added
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 // Start the server
 app.listen(PORT, () => {
