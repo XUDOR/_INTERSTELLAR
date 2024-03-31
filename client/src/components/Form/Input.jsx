8cb95175a83fc7c3bdf21cd876a202adf0366fb6
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-30 18:07:17 -0700

    fixed look of input form

--- client/src/components/Form/Input.jsx
+++ client/src/components/Form/Input.jsx
@@ -1,32 +1,43 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './Input.css';
 
-// Updated Input component to accept a callback function
 const Input = ({ onSubmit }) => {
   const [value, setValue] = useState('');
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Submitting value:", value);
-    onSubmit(value); // Call the passed-in callback function with the current value
-    setValue(''); // Optionally reset the input value
+    if (value.trim() !== '') {
+      onSubmit(value.trim());
+      setValue('');
+    } else {
+      alert('Please enter a valid value.');
+    }
   };
 
   return (
     <form onSubmit={handleSubmit} className="InputForm">
-      
-      <div className='InputTitle'>Input:</div>
-      <input
-        type="number"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        className="InputField"
-      />
-      <button type="submit" className="SubmitButton">Submit</button>
+      <div className='InputTitle'>
+        Tab #:
+        <input
+          type="number"
+          value={value}
+          onChange={handleChange}
+          className="InputField"
+          placeholder="Enter a value"
+        />
+        <button type="submit" className="SubmitButton">Submit</button>
+      </div>
     </form>
-  
   );
-  
+};
+
+Input.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
 };
 
 export default Input;
