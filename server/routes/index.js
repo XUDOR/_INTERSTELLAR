ce7fe8b7c5ce9e26df2377e0df2a5557e3706d2d
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-30 17:30:57 -0700

    admin route added to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 const albumRoutes = require('./albumRoutes');
+const adminRoutes = require('./adminRoutes'); // Import the admin routes
 //const artistRoutes = require('./artistRoutes');
 //const songRoutes = require('./songRoutes');
 // Import other routes here
 
 router.use('/albums', albumRoutes);
+router.use('/admin', adminRoutes);
 //router.use('/artists', artistRoutes);
 //router.use('/songs', songRoutes);
 // Use other routes here
