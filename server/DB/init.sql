44a94ac07ac6f09aec7444d33a8291faf2c4ee0d
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-30 17:29:34 -0700

    init SQL file for multiple sql execution

---
+++ server/DB/init.sql
@@ -0,0 +1,10 @@
+-- init.sql
+
+-- Include schema files
+\i schema/schema_1.sql
+\i schema/schema_2.sql
+
+-- Include seed data files
+\i seeds/artist_seed1.sql
+\i seeds/album_seed1.sql
+\i seeds/song_seed1.sql
