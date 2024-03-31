d923017cb2578749981a5eaff8162d74acd906fc
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-30 20:33:00 -0700

    seeds albums 2-3

--- server/DB/seeds/album_seed1.sql
+++ server/DB/seeds/album_seed1.sql
@@ -8,9 +8,7 @@ VALUES
  '2005-05-01', 
  '2010-10-03', 
  1, 
- 'The debut ambient electronic album by Roderick Shoolbraid, 
- written in a span from 2003 to 2005. Released in 2010.  
- A soundtrack for a dream, and a film that never was.', 
+ 'The debut ambient electronic album by Roderick Shoolbraid, written in a span from 2003 to 2005. Released in 2010. A soundtrack for a dream, and a film that never was.', 
  8);
 
 -- Insert record with ID 2
@@ -23,11 +21,7 @@ VALUES
  '2006-03-01', 
  '2010-10-10', 
  1, 
- 'Between "Charlotta" & "Glass City of Us" (2004-6), 
- this darker album explored minimal ambient music. 
- It used noise, line hum, record skips, and drone reverbs. 
- Inspired by the idea that objects in math, physics, & chemistry 
- have their own music, if we listen closely.', 
+ 'Between "Charlotta" & "Glass City of Us" (2004-6), this darker album explored minimal ambient music. It used noise, line hum, record skips, and drone reverbs. Inspired by the idea that objects in math, physics, & chemistry have their own music, if we listen closely.', 
  6);
 
 -- Insert record with ID 3
@@ -40,7 +34,5 @@ VALUES
  '2005-4-1', 
  '2010-10-06', 
  1, 
- 'This album became the sequel to "Charlotta", composed and engineered over a 2 year period from 2003-2005, 
- finished in the middle of 2005. The concept of "The City" emerged through the rough romantic plot of two people 
- in a massive futuristic city, that seems dwarfed by the sentiments by them both.', 
+ 'This album became the sequel to "Charlotta", composed and engineered over a 2 year period from 2003-2005, finished in the middle of 2005. The concept of "The City" emerged through the rough romantic plot of two people in a massive futuristic city, that seems dwarfed by the sentiments by them both.', 
  6);
