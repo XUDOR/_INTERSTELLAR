e10a1e318523f2bf6ac44583d34eb4b60c1fee66
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 16:43:04 -0700

    query service [ALL,byID]

--- server/services/albumService.js
+++ server/services/albumService.js
@@ -1,5 +1,15 @@
 const pool = require('../db'); // Import the pool
 
+const getAllAlbums = async () => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM albums');
+    return rows;
+  } catch (err) {
+    console.error('Error fetching all albums:', err);
+    throw new Error('Failed to fetch albums');
+  }
+};
+
 const getAlbumById = async (id) => {
   const { rows } = await pool.query('SELECT * FROM albums WHERE id = $1', [id]);
   return rows;
@@ -7,4 +17,5 @@ const getAlbumById = async (id) => {
 
 module.exports = {
   getAlbumById,
+  getAllAlbums
 };
