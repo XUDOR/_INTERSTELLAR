d91ee0e955ebd5bf3852f2e8e45e7af64a556cf4
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 11:50:46 -0700

    MusicDataContext refector

--- client/src/components/MainSection/Main.jsx
+++ client/src/components/MainSection/Main.jsx
@@ -1,53 +1,36 @@
-//Main.jsx
 import React, { useState } from 'react';
+import { useMusicData } from '../../Contexts/MusicDataContext'; // Adjust the path as necessary
 import GalleryDisplay from '../Gallery/GalleryDisplay';
-import TabList from './TabList'; 
-import Current from '../Current/Current'; 
+import TabList from './TabList';
+import Current from '../Current/Current';
 import SongList from '../Content/Album/SongList';
 import InfoContainer from '../Content/Album/InfoContainer';
-import { useTabs } from '../../Contexts/TabContext'; // Ensure the path matches the actual location
 import './Main.css';
 
 function Main() {
-  // Now also destructure adminModified from useTabs
-  const { tabLabels, adminModified } = useTabs(); 
-  const [activeTab, setActiveTab] = useState('1');
+  const { albumIndex } = useMusicData();
+  const [activeTab, setActiveTab] = useState(null); // Initially, no tab is selected
 
-  console.log("Main rendered with tabLabels:", tabLabels, "and activeTab:", activeTab, "adminModified:", adminModified);
+  // Generate tab labels dynamically from albumIndex
+  const tabLabels = Object.values(albumIndex).map(album => album.name);
 
-  const handleTabClick = (label) => {
-    console.log("Tab clicked:", label);
-    setActiveTab(label);
-  };
-
-  // Function to ensure the "album" in label 1 is not overwritten
-  const getValidatedTabLabels = () => {
-    if (adminModified && tabLabels.length >= 1 && tabLabels[0] !== "Album") {
-      return ["Album", ...tabLabels.slice(1)];
+  const handleTabClick = (albumName) => {
+    // Find album by name to set as active tab. This assumes album names are unique.
+    const album = Object.values(albumIndex).find(album => album.name === albumName);
+    if (album) {
+      setActiveTab(album.id); // Use album ID as the identifier for active tabs
     }
-    return tabLabels;
   };
 
   return (
     <div className='Main'>
-      {adminModified ? (
-        // Use getValidatedTabLabels to ensure the "Album" in tab 1 is preserved
-        <TabList activeTab={activeTab} onTabClick={handleTabClick} labels={getValidatedTabLabels()} />
-      ) : (
-        // Default setup before admin modification
-        <>
-          <TabList activeTab={activeTab} onTabClick={handleTabClick} labels={tabLabels} />
-          <GalleryDisplay activeTab={activeTab} />
-          <SongList />
-          <InfoContainer />
-          <Current />
-        </>
-      )}
+      <TabList activeTab={activeTab} onTabClick={handleTabClick} labels={tabLabels} />
+      <GalleryDisplay activeTab={activeTab} />
+      <SongList />
+      <InfoContainer />
+      <Current />
     </div>
   );
 }
 
 export default Main;
-
-
-
