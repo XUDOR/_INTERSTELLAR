cc189a3520fdd59b0a78187824a7f3901678e843
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 22:53:10 -0700

    songlist.css

--- client/src/components/Content/Album/SongList.jsx
+++ client/src/components/Content/Album/SongList.jsx
@@ -1,13 +1,12 @@
 // SongList.jsx
 import React from 'react';
 import Song from './Song';
-import './SongList.css';
 
 function SongList({ songs }) {
   return (
     <div className='SongList'>
       {songs.map((song) => (
-        <Song key={song.id} {...song} /> // Assuming `Song` can handle individual song data
+        <Song key={song.id} song={song} />
       ))}
     </div>
   );
