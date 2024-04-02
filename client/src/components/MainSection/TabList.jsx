78eacce3d281a3b23a0774ffb5cdc26c76bee160
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 19:46:06 -0700

    console log

--- client/src/components/MainSection/TabList.jsx
+++ client/src/components/MainSection/TabList.jsx
@@ -1,15 +1,20 @@
-// /MainSection/TabList.jsx
 import React from 'react';
 import './TabList.css';
 
 const TabList = ({ labels, activeTab, onTabClick }) => {
+  console.log("TabList Component: labels", labels);
+  console.log("TabList Component: activeTab", activeTab);
+
   return (
     <ul className="tab-list">
       {labels.map(({ id, name }) => (
         <li 
-          className={id === activeTab ? 'tab-list-item active' : 'tab-list-item'}
+          className={id.toString() === activeTab.toString() ? 'tab-list-item active' : 'tab-list-item'}
           key={id}
-          onClick={() => onTabClick(id)}
+          onClick={() => {
+            console.log("TabList Component: Tab clicked", id);
+            onTabClick(id);
+          }}
         >
           {name}
         </li>
