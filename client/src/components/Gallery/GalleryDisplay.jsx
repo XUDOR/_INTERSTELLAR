b2972d7605f46d4df4da66328365cb43b6610ee1
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 19:45:47 -0700

    moving to dynamic rendering

--- client/src/components/Gallery/GalleryDisplay.jsx
+++ client/src/components/Gallery/GalleryDisplay.jsx
@@ -1,38 +1,33 @@
-// GalleryDisplay.jsx
 import React from 'react';
+import { useMusicData } from '../../Contexts/MusicDataContext'; // Adjust the path as necessary
 import Gallery from './Gallery';
 import GalleryItem from './GalleryItem';
 import Album from '../Content/Album/Album';
-import AlbumTitle from '../Content/Album/AlbumTitle'; // Import AlbumTitle component
+import AlbumTitle from '../Content/Album/AlbumTitle';
 import '../Content/Album/AlbumTitle.css';
 import './GalleryDisplay.css';
 import './GalleryItem.css';
 
-function GalleryDisplay({ activeTab, setActiveTab }) {
-  console.log("GalleryDisplay showing content for tab:", activeTab);
-  // Function to handle tab selection change
-  const handleTabClick = (label) => {
-    setActiveTab(label);
-  };
+function GalleryDisplay({ activeTab }) {
+  const { albumIndex } = useMusicData();
+
+  console.log("GalleryDisplay: activeTab", activeTab);
+  console.log("GalleryDisplay: albumIndex", albumIndex);
+
+  // Function is no longer needed if the activeTab is handled by parent component
+  // const handleTabClick = (label) => {
+  //   setActiveTab(label);
+  // };
 
   return (
     <div className="GalleryDisplay">
-      <Gallery activeTab={activeTab} onTabClick={handleTabClick}>
-        <GalleryItem label="1">
-          <AlbumTitle albumId={1} /> {/* Pass album ID 1 for the first tab */}
-          
-          <Album />
-        </GalleryItem>
-        <GalleryItem label="2">
-          <AlbumTitle albumId={2} /> {/* Pass album ID 2 for the second tab */}
-          
-          {/* Other content for tab 2 */}
-        </GalleryItem>
-        <GalleryItem label="3">
-          <AlbumTitle albumId={3} /> {/* Pass album ID 3 for the third tab */}
-          
-          {/* Other content for tab 3 */}
-        </GalleryItem>
+      <Gallery activeTab={activeTab}>
+        {Object.values(albumIndex).map((album) => (
+          <GalleryItem key={album.id} label={album.id.toString()}>
+            <AlbumTitle albumId={album.id} />
+            <Album albumId={album.id} />
+          </GalleryItem>
+        ))}
       </Gallery>
     </div>
   );
