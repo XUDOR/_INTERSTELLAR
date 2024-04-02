dd35e0a5017ed0fab0bfeb9d56b21b965e797626
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 22:54:07 -0700

    refactor

--- client/src/components/MainSection/Main.jsx
+++ client/src/components/MainSection/Main.jsx
@@ -1,3 +1,4 @@
+// Main.jsx
 import React, { useState, useEffect } from 'react';
 import { useMusicData } from '../../Contexts/MusicDataContext';
 import GalleryDisplay from '../Gallery/GalleryDisplay';
@@ -8,22 +9,19 @@ import InfoContainer from '../Content/Album/InfoContainer';
 import './Main.css';
 
 function Main() {
-  const { albumIndex, songIndex } = useMusicData(); // Assuming songIndex is structured similarly
+  const { albumIndex, songIndex } = useMusicData();
   const [activeTab, setActiveTab] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (Object.keys(albumIndex).length > 0) {
-      setActiveTab(Object.keys(albumIndex)[0]);
+    if (Object.keys(albumIndex).length > 0 && Object.keys(songIndex).length > 0) {
+      setActiveTab(Object.keys(albumIndex)[0]); // Default to first album
       setIsLoading(false);
     }
-  }, [albumIndex]);
+  }, [albumIndex, songIndex]);
 
   const tabLabels = Object.values(albumIndex).map(({ id, name }) => ({ id, name }));
-
-  // Assuming songIndex maps album IDs to song lists
-  const activeSongs = activeTab ? songIndex[activeTab] : [];
-
+  
   const handleTabClick = (albumId) => {
     setActiveTab(albumId);
   };
@@ -36,12 +34,12 @@ function Main() {
     <div className='Main'>
       <TabList activeTab={activeTab} onTabClick={handleTabClick} labels={tabLabels} />
       <GalleryDisplay activeTab={activeTab} />
-      <SongList songs={activeSongs} /> {/* Pass activeSongs to SongList */}
+      {/* Assuming you want to display songs for the active album */}
+      <SongList songs={Object.values(songIndex).filter(song => song.albumId === parseInt(activeTab))} />
       <InfoContainer />
       <Current />
     </div>
   );
 }
 
-
-export default Main;    
+export default Main;
