8b1ada45b40e4077fc352cebcbf0cc6f1b6f3e4b
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 19:45:26 -0700

    refactor and console.log

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,23 +1,27 @@
-// Gallery.jsx
 import React from 'react';
 import './Gallery.css';
 
 const Gallery = ({ children, activeTab, onTabClick }) => {
+  console.log("Gallery Component: Rendering with activeTab", activeTab);
+
+  // If children is not an array (e.g., when there's only one child), wrap it in an array
+  const childrenArray = React.Children.toArray(children);
+
   return (
     <div className="Gallery">
-      {/* ... Other components or markup */}
       <div className="tab-content">
-        {children.map((one) => {
-          const display = one.props.label === activeTab ? 'block' : 'none'; // Only display the active tab content
+        {childrenArray.map((child) => {
+          const display = child.props.label === activeTab ? 'block' : 'none';
+          console.log(`Gallery Component: Displaying tab ${child.props.label} as ${display}`);
           return (
-            <div key={one.props.label} style={{ display }}>
-              {one.props.children}
+            <div key={child.props.label} style={{ display }}>
+              {child.props.children}
             </div>
           );
         })}
       </div>
     </div>
   );
-};
+}
 
 export default Gallery;
