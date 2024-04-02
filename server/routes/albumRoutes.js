e95af575d15462aeb21f97493add2e7de997509e
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 17:42:23 -0700

    console log trace

--- server/routes/albumRoutes.js
+++ server/routes/albumRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db')
 
-const { getAlbumById, getAllAlbums } = require('../services/albumService'); // Corrected import
+const { getAlbumById, getAllAlbums } = require('../services/albumService');
 
 // Fetch all albums
 router.get('/', async (req, res) => {
+  console.log('Received request to fetch all albums');
   try {
     const albums = await getAllAlbums();
+    console.log('Fetched albums:', albums);
     res.json(albums);
   } catch (err) {
     console.error('Error fetching all albums:', err);
@@ -18,15 +19,18 @@ router.get('/', async (req, res) => {
 // Fetch an album by ID
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
+  console.log(`Received request to fetch album with ID: ${id}`);
   try {
-    const rows = await getAlbumById(id);
-    if (rows.length > 0) {
-      res.json(rows[0]);
+    const albums = await getAlbumById(id);
+    if (albums.length > 0) {
+      console.log('Fetched album:', albums[0]);
+      res.json(albums[0]);
     } else {
+      console.log(`Album with ID: ${id} not found`);
       res.status(404).send('Album not found');
     }
   } catch (err) {
-    console.error('Error fetching album:', err);
+    console.error(`Error fetching album with ID: ${id}`, err);
     res.status(500).send('Server error');
   }
 });
