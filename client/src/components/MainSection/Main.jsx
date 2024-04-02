5fc5d37f54c630e5e75d2330056d911844b8bec9
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 19:45:55 -0700

    console log

--- client/src/components/MainSection/Main.jsx
+++ client/src/components/MainSection/Main.jsx
@@ -9,11 +9,16 @@ import './Main.css';
 
 function Main() {
   const { albumIndex } = useMusicData();
+  console.log("Main Component: albumIndex", albumIndex);
+
   const [activeTab, setActiveTab] = useState(Object.keys(albumIndex)[0]); // Use first album ID as default
+  console.log("Main Component: activeTab", activeTab);
 
   const tabLabels = Object.values(albumIndex).map(({ id, name }) => ({ id, name }));
+  console.log("Main Component: tabLabels", tabLabels);
 
   const handleTabClick = (albumId) => {
+    console.log("Main Component: tab clicked", albumId);
     setActiveTab(albumId); // Directly use album ID
   };
 
