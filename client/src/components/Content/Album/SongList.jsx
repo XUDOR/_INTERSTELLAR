15ffe2a0df1e8b977f12b966d445a5c18277a237
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-02 16:57:34 -0700

    name removal

--- client/src/components/Content/Album/SongList.jsx
+++ client/src/components/Content/Album/SongList.jsx
@@ -5,6 +5,7 @@ import Song from './Song';
 function SongList({ songs }) {
   return (
     <div className='SongList'>
+      songlist
       {songs.map((song) => (
         <Song key={song.id} song={song} />
       ))}
