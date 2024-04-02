bbe4f6cda897fda7ee203c25a3f1b80f7e345459
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 22:30:47 -0700

    refactor -- chasing down tab bug

--- client/src/components/Gallery/GalleryDisplay.jsx
+++ client/src/components/Gallery/GalleryDisplay.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
-import { useMusicData } from '../../Contexts/MusicDataContext'; // Adjust the path as necessary
+import { useMusicData } from '../../Contexts/MusicDataContext';
 import Gallery from './Gallery';
 import GalleryItem from './GalleryItem';
 import Album from '../Content/Album/Album';
 import AlbumTitle from '../Content/Album/AlbumTitle';
-import '../Content/Album/AlbumTitle.css';
-import './GalleryDisplay.css';
-import './GalleryItem.css';
+import SongList from '../Content/Album/SongList'; // Assuming this component exists
 
 function GalleryDisplay({ activeTab }) {
-  const { albumIndex } = useMusicData();
-
-  console.log("GalleryDisplay: activeTab", activeTab);
-  console.log("GalleryDisplay: albumIndex", albumIndex);
-
-  // Function is no longer needed if the activeTab is handled by parent component
-  // const handleTabClick = (label) => {
-  //   setActiveTab(label);
-  // };
+  const { albumIndex, songIndex } = useMusicData();
+  
+  // Active album data
+  const activeAlbum = albumIndex[activeTab];
+  
+  // Songs for the active album
+  const songsForActiveAlbum = songIndex[activeTab] || [];
 
   return (
     <div className="GalleryDisplay">
-      <Gallery activeTab={activeTab}>
-        {Object.values(albumIndex).map((album) => (
-          <GalleryItem key={album.id} label={album.id.toString()}>
-            <AlbumTitle albumId={album.id} />
-            <Album albumId={album.id} />
+      {activeAlbum ? (
+        <Gallery activeTab={activeTab}>
+          {/* Single GalleryItem for the active album */}
+          <GalleryItem key={activeAlbum.id} label={activeAlbum.id.toString()}>
+            <AlbumTitle albumId={activeAlbum.id} />
+            <Album albumId={activeAlbum.id} />
+            {/* Displaying songs for the active album */}
+            <SongList songs={songsForActiveAlbum} />
           </GalleryItem>
-        ))}
-      </Gallery>
+        </Gallery>
+      ) : (
+        <div>Loading or no album selected...</div>
+      )}
     </div>
   );
 }
