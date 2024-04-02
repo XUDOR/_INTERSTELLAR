8a076caf3bd1ef4868e1fc9401aaf1860c7309e5
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-01 19:44:48 -0700

    console log

--- client/src/components/Content/Album/AlbumTitle.jsx
+++ client/src/components/Content/Album/AlbumTitle.jsx
@@ -1,15 +1,18 @@
-// AlbumTitle.jsx
 import React, { useContext } from 'react';
 import { MusicDataContext } from '../../../Contexts/MusicDataContext'; // Adjust the import path as necessary
 
 function AlbumTitle({ albumId }) {
   const { albumIndex } = useContext(MusicDataContext);
   
+  // Log to check the album being accessed
+  console.log(`AlbumTitle Component: Fetching album with ID ${albumId}`, albumIndex[albumId]);
+
   // Access the album directly from the albumIndex using the albumId
   const album = albumIndex[albumId];
 
   // If the album data for the given ID isn't found, show a placeholder message
   if (!album) {
+    console.error(`AlbumTitle Component: Album with ID ${albumId} not found.`);
     return <div>Loading or album not found...</div>;
   }
 
