f09134920545f13c95e4fad4dfe07eb2df577a0a
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-03 11:51:58 -0700

    console logs

--- client/src/components/MainSection/Main.jsx
+++ client/src/components/MainSection/Main.jsx
@@ -7,32 +7,40 @@ import InfoContainer from '../Content/Album/InfoContainer';
 import './Main.css';
 
 function Main() {
-  const { albumIndex } = useMusicData(); // Removed songIndex since it's no longer directly used in Main
+  const { albumIndex } = useMusicData();
   const [activeTab, setActiveTab] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  // Log the state of albumIndex on each render
+  console.log("Main component render, albumIndex:", albumIndex);
+
   useEffect(() => {
+    console.log("Main useEffect for albumIndex:", albumIndex);
     if (Object.keys(albumIndex).length > 0) {
-      setActiveTab(Object.keys(albumIndex)[0]); // Default to the first album
+      const firstAlbumId = Object.keys(albumIndex)[0];
+      console.log("Setting activeTab to the first albumId:", firstAlbumId);
+      setActiveTab(firstAlbumId); // Default to the first album
       setIsLoading(false);
     }
-  }, [albumIndex]); // Removed songIndex from the dependency array since it's no longer used here
+  }, [albumIndex]);
 
   const tabLabels = Object.values(albumIndex).map(({ id, name }) => ({ id, name }));
-  
+
   const handleTabClick = (albumId) => {
+    console.log("Tab clicked, setting activeTab:", albumId);
     setActiveTab(albumId);
   };
 
   if (isLoading) {
+    console.log("Main component displaying loading state.");
     return <div>Loading...</div>;
   }
 
+  console.log("Main component rendering content, activeTab:", activeTab);
   return (
     <div className='Main'>
       <TabList activeTab={activeTab} onTabClick={handleTabClick} labels={tabLabels} />
       <GalleryDisplay activeTab={activeTab} />
-      
       <InfoContainer />
       <Current />
     </div>
