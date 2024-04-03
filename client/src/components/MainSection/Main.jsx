6341cabe10b79661e000b7c5d1597c20918767a4
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-02 17:43:42 -0700

    removed songlist from main // songlist takes props from GalleryDisplay.jsx

--- client/src/components/MainSection/Main.jsx
+++ client/src/components/MainSection/Main.jsx
@@ -1,24 +1,22 @@
-// Main.jsx
 import React, { useState, useEffect } from 'react';
 import { useMusicData } from '../../Contexts/MusicDataContext';
 import GalleryDisplay from '../Gallery/GalleryDisplay';
 import TabList from './TabList';
 import Current from '../Current/Current';
-import SongList from '../Content/Album/SongList';
 import InfoContainer from '../Content/Album/InfoContainer';
 import './Main.css';
 
 function Main() {
-  const { albumIndex, songIndex } = useMusicData();
+  const { albumIndex } = useMusicData(); // Removed songIndex since it's no longer directly used in Main
   const [activeTab, setActiveTab] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (Object.keys(albumIndex).length > 0 && Object.keys(songIndex).length > 0) {
-      setActiveTab(Object.keys(albumIndex)[0]); // Default to first album
+    if (Object.keys(albumIndex).length > 0) {
+      setActiveTab(Object.keys(albumIndex)[0]); // Default to the first album
       setIsLoading(false);
     }
-  }, [albumIndex, songIndex]);
+  }, [albumIndex]); // Removed songIndex from the dependency array since it's no longer used here
 
   const tabLabels = Object.values(albumIndex).map(({ id, name }) => ({ id, name }));
   
@@ -34,8 +32,6 @@ function Main() {
     <div className='Main'>
       <TabList activeTab={activeTab} onTabClick={handleTabClick} labels={tabLabels} />
       <GalleryDisplay activeTab={activeTab} />
-      {/* Assuming you want to display songs for the active album */}
-      <SongList songs={Object.values(songIndex).filter(song => song.albumId === parseInt(activeTab))} />
       <InfoContainer />
       <Current />
     </div>
