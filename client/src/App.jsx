3823c0813332148e6a7cf2fe3e9fc67408edf8db
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-03 11:51:20 -0700

    refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
-//app,jsx 
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import LoadingPage from './components/Loading/LoadingPage';
 import Header from './components/Header/Header';
 import Main from './components/MainSection/Main';
 import Footer from './components/Footer/Footer';
@@ -17,25 +16,41 @@ import './App.css';
 
 const App = () => {
   const { isMinimal } = useHeaderStyle();
+  const [isLoading, setIsLoading] = useState(true); // Initial loading state for the app
+
+  useEffect(() => {
+    // Simulate a loading time of 1-2 seconds (1500 milliseconds as an example)
+    const timer = setTimeout(() => {
+      setIsLoading(false); // Hide the loading page and show the app content
+    }, 1500);
+
+    // Cleanup function to clear the timer if the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    // Show the LoadingPage component while the app is in the loading state
+    return <LoadingPage />;
+  }
 
   return (
-      <MusicDataProvider>
-        <div className="app">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/shop" element={<ShopPage />} />
-              <Route path="/admin" element={<AdminPage />} />
-              <Route path="/user" element={<UserPage />} />
-              <Route path="/downloads" element={<DownloadsPage />} />
-              <Route path="/epk" element={<EPKPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-            </Routes>
-          </main>
-          {!isMinimal && <Footer />}
-        </div>
-        </MusicDataProvider>
+    <MusicDataProvider>
+      <div className="app">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/user" element={<UserPage />} />
+            <Route path="/downloads" element={<DownloadsPage />} />
+            <Route path="/epk" element={<EPKPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        {!isMinimal && <Footer />}
+      </div>
+    </MusicDataProvider>
   );
 };
 
