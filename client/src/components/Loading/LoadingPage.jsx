19686c0b7466532794eb200f4903caca73405167
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-03 11:51:45 -0700

    loading page start

---
+++ client/src/components/Loading/LoadingPage.jsx
@@ -0,0 +1,15 @@
+// AlbumPage.jsx
+import React from "react";
+import './LoadingPage.css';
+
+function LoadingPage () {
+  return (
+    <>
+    <div className="LoadingContent">
+     <div className="LoadingTitle">...LOADING...</div> 
+    </div>
+    </>
+  );
+}
+
+export default LoadingPage;
