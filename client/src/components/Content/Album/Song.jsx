6330ce0cefa2db5ef1869cdd591669f53b068e56
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-03 08:05:57 -0700

    adding props for fetch

--- client/src/components/Content/Album/Song.jsx
+++ client/src/components/Content/Album/Song.jsx
@@ -2,13 +2,16 @@
 import React from 'react';
 import './Song.css'
 
+// Adjusting the Song component to match your data structure
 function Song({ song }) {
   return (
     <div className='Song'>
-      <div>{song.title} - {song.artist}</div>
+      {/* Assuming `name` is the correct property based on your state log */}
+      <div>{song.name}</div>
       {/* Display more song details here */}
     </div>
   );
 }
+
 
 export default Song;
