296c1453118a5d103eced90aa1d4a4ff2fccf08a
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-04 14:57:02 -0700

    deBud-refactor

--- client/src/components/Content/Album/Cover.jsx
+++ client/src/components/Content/Album/Cover.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './Cover.css';
 
 function Cover({ imageUrl }) {
+  console.log('Cover component trying to load image URL:', imageUrl); // Log the image URL being loaded
+
   return (
-    <div className="cover-container"> {/* Container div with a class for styling */}
+    <div className="cover-container">
       <img 
         src={imageUrl} 
         alt="Album Cover" 
         onError={(e) => { 
+          console.error('Failed to load image, setting fallback:', imageUrl); // Log on image load failure
           e.target.onerror = null; // Prevent further onError triggers
           e.target.src = '/Fallback-image.png'; // Directly reference from the public folder
+          console.log('Fallback image src set to:', e.target.src); // Log fallback image src
         }} 
       />
     </div>
