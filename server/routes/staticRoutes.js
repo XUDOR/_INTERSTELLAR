893c6070cdecde412a07fb971d7c0d097b371498
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-04 12:05:59 -0700

    update for retrival

--- server/routes/staticRoutes.js
+++ server/routes/staticRoutes.js
@@ -1,16 +1,26 @@
+// /routes/StaticRoutes.js
+
 const express = require('express');
 const path = require('path');
+const ImageService = require('../services/imageService'); // Adjust the path as needed
 const router = express.Router();
 
-// Inside your image route
-router.get('/image', (req, res) => {
-  // Note: Adjust the path according to where your image is located inside the public folder
-  const imagePath = path.join(__dirname, '..', '..', 'public', 'CHARLOTTA_cover.png');
+router.get('/images/:albumId', async (req, res) => {
+  const { albumId } = req.params;
+  
+  const imageName = ImageService.getImageNameByAlbumId(albumId);
 
+  if (!imageName) {
+    return res.status(404).send('Image not found');
+  }
 
-
-
-  res.sendFile(imagePath);
+  const imagePath = path.join(__dirname, '..', '..', 'src', 'images', 'Covers', imageName);
+  res.sendFile(imagePath, (err) => {
+    if (err) {
+      console.log(err); // Log for debugging
+      res.status(404).send('Image not found');
+    }
+  });
 });
 
 module.exports = router;
