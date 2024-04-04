9d0d27b56bd68c23c3106f8c56ddb420371e3fc8
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-04 11:56:10 -0700

    refactor for images from backend

--- client/src/components/Content/Album/Album.jsx
+++ client/src/components/Content/Album/Album.jsx
@@ -1,23 +1,24 @@
+// Album.jsx
+
 import React from 'react';
-import Cover from './Cover';
+import { API_BASE_URL } from '../../../utils/const';
+import Cover from './Cover'; // Adjust the import path as necessary
 import './Album.css';
-// Uncomment below when ready for dynamic image fetching
-// import useAuthenticatedImage from '../../../hooks/useAuthenticatedImage';
-import coverImage from '../../../images/Covers/CHARLOTTA_cover.png';
 
-function Album({ albumId }) { // Assuming albumId might be used for fetching specific album images
-  // For dynamic image fetching, uncomment the line below
-  // const coverImageUrl = useAuthenticatedImage(albumId);
-  const coverImageUrl = coverImage; // Use the imported image for now
+function Album({ albumId }) {
+  // Construct the URL to the image served by the backend using the constant
+  const imageUrl = `${API_BASE_URL}/api/images/${albumId}`;
 
-  console.log(`Album Component: Displaying album with ID ${albumId} using cover image URL`, coverImageUrl);
+  console.log(`Fetching album cover for album ID ${albumId} from: ${imageUrl}`);
 
   return (
-    <div className='Album'>
-      {/* Pass the image URL to the Cover component */}
-      <Cover imageUrl={coverImageUrl} />
+    <div className="album">
+      {/* Use the Cover component and pass the imageUrl */}
+      <Cover imageUrl={imageUrl} />
     </div>
   );
 }
 
 export default Album;
+
+
