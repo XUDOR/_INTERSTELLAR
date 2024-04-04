c27fb73ee0b0aae43105b0926887545c2bd77daf
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-03 19:51:04 -0700

    song seeds ablums 2-5

--- server/DB/seeds/song_seed1.sql
+++ server/DB/seeds/song_seed1.sql
@@ -1,12 +1,54 @@
 --song_seed1.sql
 
-INSERT INTO songs (id, name, audio_url, artist_id, album_id, track_id) 
+INSERT INTO songs (id, name, audio_url, duration, artist_id, album_id, track_id) 
 VALUES 
-('0101', 'Charlotta', 'http://example.com/audio/song1.mp3', 1, 1, 1),
-('0102', 'Garden Mix', 'http://example.com/audio/song2.mp3', 1, 1, 2),
-('0103', 'New Ornithologies', 'http://example.com/audio/song3.mp3', 1, 1, 3),
-('0104', 'Old Flowers', 'http://example.com/audio/song4.mp3', 1, 1, 4),
-('0105', 'Windows', 'http://example.com/audio/song5.mp3', 1, 1, 5),
-('0106', 'Beautiful Waters', 'http://example.com/audio/song6.mp3', 1, 1, 6),
-('0107', 'Yellow Transports', 'http://example.com/audio/song7.mp3', 1, 1, 7),
-('0108', 'Perfumed Letter', 'http://example.com/audio/song8.mp3', 1, 1, 8);
+
+--Charlotta--
+
+('0101', 'Charlotta', 'http://example.com/audio/song1.mp3', '00:08:28', 1, 1, 1),
+('0102', 'Garden Mix', 'http://example.com/audio/song2.mp3', '00:03:28', 1, 1, 2),
+('0103', 'New Ornithologies', 'http://example.com/audio/song3.mp3', '00:03:51', 1, 1, 3),
+('0104', 'Old Flowers', 'http://example.com/audio/song4.mp3', '00:03:33', 1, 1, 4),
+('0105', 'Windows', 'http://example.com/audio/song5.mp3', '00:09:12', 1, 1, 5),
+('0106', 'Beautiful Waters', 'http://example.com/audio/song6.mp3', '00:08:05', 1, 1, 6),
+('0107', 'Yellow Transports', 'http://example.com/audio/song7.mp3', '00:04:39', 1, 1, 7),
+('0108', 'Perfumed Letter', 'http://example.com/audio/song8.mp3', '00:04:49', 1, 1, 8),
+
+--Objects & Particles--
+
+('0201', 'Jade Flower Memory Index', 'http://example.com/audio/song1.mp3', '00:06:19', 1, 2, 1),
+('0202', 'r+d=t', 'http://example.com/audio/song2.mp3', '00:09:06', 1, 2, 2),
+('0203', 'Raintunnels', 'http://example.com/audio/song3.mp3', '00:04:35', 1, 2, 3),
+('0204', 'Snow Crystal', 'http://example.com/audio/song4.mp3', '00:08:09', 1, 2, 4),
+('0205', 'X+Y=Z', 'http://example.com/audio/song5.mp3', '00:06:10', 1, 2, 5),
+('0206', 'V', 'http://example.com/audio/song6.mp3', '00:06:45', 1, 2, 6),
+
+--Glass City of Us--
+
+('0301', 'Caterpillar', 'http://example.com/audio/song1.mp3', '00:06:43', 1, 3, 1),
+('0302', 'Films of Nature', 'http://example.com/audio/song2.mp3', '00:06:09', 1, 3, 2),
+('0303', 'Glass City of Us', 'http://example.com/audio/song3.mp3', '00:12:52', 1, 3, 3),
+('0304', 'Nice', 'http://example.com/audio/song4.mp3', '00:07:00', 1, 3, 4),
+('0305', 'Optical Record Pipe', 'http://example.com/audio/song5.mp3', '00:07:44', 1, 3, 5),
+('0306', 'Sun Through Clouds onto Flowers', 'http://example.com/audio/song6.mp3', '00:08:33', 1, 3, 6),
+
+--New Domes of Earth--
+
+('0401', 'Beaches', 'http://example.com/audio/song1.mp3', '00:03:58', 1, 4, 1),
+('0402', 'Projected Mountains', 'http://example.com/audio/song2.mp3', '00:08:24', 1, 4, 2),
+('0403', 'Repeated Fires', 'http://example.com/audio/song3.mp3', '00:03:04', 1, 4, 3),
+('0404', 'The Taste of Truth', 'http://example.com/audio/song4.mp3', '00:04:38', 1, 4, 4),
+('0405', 'Spheres of Life', 'http://example.com/audio/song5.mp3', '00:02:54', 1, 4, 5),
+('0406', 'Dawn for Children', 'http://example.com/audio/song6.mp3', '00:04:29',1, 4, 6),
+
+--Natura--
+
+('0501', 'Botany', 'http://example.com/audio/song1.mp3', '00:03:19', 1, 5, 1),
+('0502', 'Clouds', 'http://example.com/audio/song2.mp3', '00:05:12', 1, 5, 2),
+('0503', 'Colours in Rain', 'http://example.com/audio/song3.mp3', '00:07:18', 1, 5, 3),
+('0504', 'Early Summer Sky', 'http://example.com/audio/song4.mp3', '00:04:46', 1, 5, 4),
+('0505', 'First Spring', 'http://example.com/audio/song5.mp3', '00:05:26', 1, 5, 5),
+('0506', 'Flower', 'http://example.com/audio/song6.mp3', '00:03:45',1, 5, 6),
+('0507', 'Streams', 'http://example.com/audio/song6.mp3', '00:05:29',1, 5, 7),
+('0508', 'Tidal', 'http://example.com/audio/song6.mp3', '00:04:55',1, 5, 8),
+('0509', 'Waters', 'http://example.com/audio/song6.mp3', '00:05:46',1, 5, 9),
