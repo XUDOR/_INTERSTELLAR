09d0977b89cd9a3092bc9b7f26e23ec5dd677868
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-04 15:27:43 -0700

    static file config

--- server/server.js
+++ server/server.js
@@ -1,27 +1,26 @@
-//server.js
-
-require('dotenv').config(); // Make sure to require dotenv at the top
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes'); // Import centralized routes
-const pool = require('./db'); // Assuming db.js is set up for global use
+const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json()); // Needed to parse JSON bodies
+app.use(express.json());
 app.use(cors());
+
+// Serve static files from the 'public' directory
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // Use centralized routes with '/api' as the base path
 app.use('/api', routes);
 
-// Error handling middleware should be the last piece of middleware added
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
