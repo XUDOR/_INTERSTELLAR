d17e700d2e8a5497aa9b1dc9bb8dc9c04c997af7
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-04 19:39:05 -0700

    console log

--- client/src/components/Content/Album/Cover.jsx
+++ client/src/components/Content/Album/Cover.jsx
@@ -1,8 +1,10 @@
+// Cover.jsx
+
 import React from 'react';
 import './Cover.css';
 
 function Cover({ imageUrl }) {
-  console.log('Cover component trying to load image URL:', imageUrl); // Log the image URL being loaded
+  console.log('Cover component trying to load image URL:', imageUrl);
 
   return (
     <div className="cover-container">
@@ -10,10 +12,10 @@ function Cover({ imageUrl }) {
         src={imageUrl} 
         alt="Album Cover" 
         onError={(e) => { 
-          console.error('Failed to load image, setting fallback:', imageUrl); // Log on image load failure
-          e.target.onerror = null; // Prevent further onError triggers
-          e.target.src = '/Fallback-image.png'; // Directly reference from the public folder
-          console.log('Fallback image src set to:', e.target.src); // Log fallback image src
+          console.error('Failed to load image, setting fallback:', imageUrl);
+          e.target.onerror = null;
+          e.target.src = `${process.env.PUBLIC_URL}/Fallback-image.png`; // Use PUBLIC_URL for images in the public folder
+          console.log('Fallback image src set to:', e.target.src);
         }} 
       />
     </div>
