5a0273d034828ab0103b124ef81ff7a5f41e347f
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-04 19:43:43 -0700

    renamed imageService // but we are using direct url from the database

--- server/services/imageService.js
+++ server/services/imageService.js
@@ -3,16 +3,24 @@
 // Placeholder for a database-driven approach or static file mapping
 const albumImageMap = {
   '1': '1_CHARLOTTA.png',
-  '2': '2_GLASS-CITY-OF-US.png',
-  '3': '3_OBJECTS-PARTICLES.png',
+  '2': '2_OBJECTS-PARTICLES.png',
+  '3': '3_GLASS-CITY-OF-US.png',
+  
   // Additional mappings...
 };
 
+const getImageNameByAlbumId = (albumId) => {
+  return albumImageMap[albumId] || null;
+}
+
+/*
 class ImageService {
   static getImageNameByAlbumId(albumId) {
     // In a real application, you might replace this with a database query
     return albumImageMap[albumId] || null;
   }
 }
+*/
 
-module.exports = ImageService;
+
+module.exports = {getImageNameByAlbumId};
