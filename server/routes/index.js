e911a79a94f8524893aa25e8c661604dba03ba5c
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-04 19:42:22 -0700

    update routes for coverRoutes

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,14 @@ const albumRoutes = require('./albumRoutes');
 const adminRoutes = require('./adminRoutes');
 //const artistRoutes = require('./artistRoutes');
 const songRoutes = require('./songRoutes');
+const coverRoutes = require('./coverRoutes');
 // Import other routes here
 
 router.use('/albums', albumRoutes);
 router.use('/admin', adminRoutes);
 //router.use('/artists', artistRoutes);
 router.use('/songs', songRoutes);
+router.use('/covers',coverRoutes);
 // Use other routes here
 
 module.exports = router;
