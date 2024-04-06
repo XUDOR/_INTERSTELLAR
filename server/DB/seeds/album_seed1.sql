f2c450cf0458151f7feaf5558fc9303cac91106a
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-06 16:58:11 -0700

    album seeding

--- server/DB/seeds/album_seed1.sql
+++ server/DB/seeds/album_seed1.sql
@@ -2,65 +2,166 @@
 
 -- Insert record with ID 1
 INSERT INTO albums 
-(id, name, cover_url, production_date, release_date, artist_id, description, tracks) 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
 VALUES 
 (1,
+ '00040101',
  'Charlotta', 
  '/static/images/Covers/1_CHARLOTTA.png',
  '2005-05-01', 
  '2010-10-03', 
- 1, 
+ 1,
+ 'All Sound & Composition: Roderick Shoolbraid. Album Cover Art, Photography & Design: Roderick Shoolbraid. All Rights Reserved.', 
  'The debut ambient electronic album by Roderick Shoolbraid, written in a span from 2003 to 2005. Released in 2010. A soundtrack for a dream, and a film that never was.', 
  8);
 
+
 -- Insert record with ID 2
 INSERT INTO albums 
-(id, name, cover_url, production_date, release_date, artist_id, description, tracks) 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks)  
 VALUES 
 (2,
+ '00040102',
  'Objects & Particles', 
  '/static/images/Covers/2_OBJECTS-PARTICLES.png',
  '2006-03-01', 
  '2010-10-10', 
- 1, 
+ 1,
+ 'All Sound & Composition: Roderick Shoolbraid. Album Cover Art, Photography & Design: Roderick Shoolbraid. All Rights Reserved', 
  'Between "Charlotta" & "Glass City of Us" (2004-6), this darker album explored minimal ambient music. It used noise, line hum, record skips, and drone reverbs. Inspired by the idea that objects in math, physics, & chemistry have their own music, if we listen closely.', 
  6);
 
 -- Insert record with ID 3
 INSERT INTO albums 
-(id, name, cover_url, production_date, release_date, artist_id, description, tracks) 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
 VALUES 
 (3,
+ '00040103'
  'Glass City of Us', 
  '/static/images/Covers/3_GLASS-CITY-OF-US.png', 
  '2005-4-1', 
  '2010-10-06', 
- 1, 
+ 1,
+ 'All Sound & Composition: Roderick Shoolbraid. Album Cover Art, Photography & Design: Roderick Shoolbraid. All Rights Reserved.', 
  'This album became the sequel to "Charlotta", composed and engineered over a 2 year period from 2003-2005, finished in the middle of 2005. The concept of "The City" emerged through the rough romantic plot of two people in a massive futuristic city, that seems dwarfed by the sentiments by them both.', 
  6);
 
 -- Insert record with ID 4
 INSERT INTO albums 
-(id, name, cover_url, production_date, release_date, artist_id, description, tracks) 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks)  
 VALUES 
 (4,
+ '00040104',
  'New Domes of Earth', 
  '/static/images/Covers/4_NEW-DOMES.png',                                        
  '2005-5-15', 
  '2010-10-19', 
- 1, 
+ 1,
+ 'All Sound & Composition: Roderick Shoolbraid. Album Cover Art, Photography & Design: Roderick Shoolbraid. All Rights Reserved.', 
  'Music for visions of the future.', 
  6);
 
  -- Insert record with ID 5
 INSERT INTO albums 
-(id, name, cover_url, production_date, release_date, artist_id, description, tracks) 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
 VALUES 
 (5,
+ '01040105',
  'Natura', 
  '/static/images/Covers/5_NATURA.png', 
  '2022-03-15', 
  '2022-03-22', 
- 1, 
+ 1,
+ 'All Sound and Instruments: Roderick Shoolbraid. Painting & Album Cover Design: Roderick Shoolbraid. All Rights Reserved.',
  'An open letter of love and admiration for the natural world.', 
- 6);
+ 9);
+
+ -- Insert record with ID 6
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(6,
+ '01040106',
+ 'Outer Corners', 
+ '/static/images/Covers/6_OUTER CORNERS.png', 
+ '2022-02-15', 
+ '2022-03-25', 
+ 1,
+ 'All Sound & Composition: Roderick Shoolbraid. Album Cover design: Roderick Shoolbraid. All Rights Reserved.',
+ 'Music for the stars and beyond. Started in late 2021, completed in 2022.', 
+ 7);
+
+ -- Insert record with ID 7
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(7,
+ '01040107',
+ 'Nonagon', 
+ '/static/images/Covers/7_NONAGON.png', 
+ '2022-05-15', 
+ '2022-06-14', 
+ 1,
+ 'All Sound, production and Composition: Roderick Shoolbraid Album Cover design: Roderick Shoolbraid All Rights Reserved.',
+ 'Lines drawn by field recordings, manipulated and accompanied by guitars & bass. Coloured in by synths and reverb to show a space of the beauty of geometry. Sound sources from: 1996-7, 2003-15.Compiled and finished in summer 2022.', 
+ 9);
+
+ -- Insert record with ID 8
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(8,
+ '01040108',
+ 'Ambient Garden One', 
+ '/static/images/Covers/8_AMBIENTGARDENONE.png', 
+ '2022-06-22', 
+ '2022-06-22', 
+ 1,
+ 'All Sound, production and Composition: Roderick Shoolbraid Album Cover design: Roderick Shoolbraid All Rights Reserved.',
+ 'A curated collection of some favourite pieces from the present discography. Spanning 20 years 2002-2022, this exposition showcases the work from a variety of different artistic perspectives: Electronica, Drone, Minimalism, Polytonality and Free Jazz. One is always a summary of influences. And in this compilation it is evident - an open letter to the history of electronic, electro-acoustic music and musique concrète. The appreciation for the natural world and Its interplay with the Astral.', 
+ 14);
+
+  -- Insert record with ID 9
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(9,
+ '01040109',
+ 'Music for Seven Structures', 
+ '/static/images/Covers/9_7STRUCTURES.png', 
+ '2022-06-01', 
+ '2022-06-28', 
+ 1,
+ 'All Sound & Composition: Roderick Shoolbraid Album Cover design: Roderick Shoolbraid All Rights Reserved.',
+ 'A simple minimal album exploring the images of places and structures that came to mind, while designing & demoing the synth patches created for Nonagon. Written from May to June 2022.', 
+ 7);
+
+  -- Insert record with ID 10
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(10,
+ '01040110',
+ 'Inner Moments of Light', 
+ '/static/images/Covers/10_INNER MOMENTS.png', 
+ '2015-11-15', 
+ '2022-07-09', 
+ 1,
+ 'All Music & Sound: Roderick Shoolbraid Album Cover Design: Roderick Shoolbraid All Rights Reserved.',
+ 'A collection or curation of assorted tracks from 2003-2015. With some source files going back as far as 1996-7. Organized finally in a little volume of what I like to think of as sacred or holy music, music or sound for meditation into specific spaces, or at least it is what it is for me. Everyone will have their own experience of the ambient medicinal sounds, finding itself roots in Minimalism, Drone, Noise, & Lo Fi. This was my original intent behind the contemplative sound, and music I created, whether I am successful, I leave that in your hands and your mind.', 
+ 9);
+
+   -- Insert record with ID 11
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(11,
+ '01040111',
+ 'Inner Moments of Light', 
+ '/static/images/Covers/10_INNER MOMENTS.png', 
+ '2015-11-15', 
+ '2022-07-09', 
+ 1,
+ 'All Music & Sound: Roderick Shoolbraid Album Cover Design: Roderick Shoolbraid All Rights Reserved.',
+ 'A collection or curation of assorted tracks from 2003-2015. With some source files going back as far as 1996-7. Organized finally in a little volume of what I like to think of as sacred or holy music, music or sound for meditation into specific spaces, or at least it is what it is for me. Everyone will have their own experience of the ambient medicinal sounds, finding itself roots in Minimalism, Drone, Noise, & Lo Fi. This was my original intent behind the contemplative sound, and music I created, whether I am successful, I leave that in your hands and your mind.', 
+ 9);
