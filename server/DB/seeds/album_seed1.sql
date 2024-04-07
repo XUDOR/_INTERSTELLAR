b25459ab7b29a2419ebbd4653c9c0feec07d4c97
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-06 18:23:10 -0700

    seed update

--- server/DB/seeds/album_seed1.sql
+++ server/DB/seeds/album_seed1.sql
@@ -151,17 +151,160 @@ VALUES
  'A collection or curation of assorted tracks from 2003-2015. With some source files going back as far as 1996-7. Organized finally in a little volume of what I like to think of as sacred or holy music, music or sound for meditation into specific spaces, or at least it is what it is for me. Everyone will have their own experience of the ambient medicinal sounds, finding itself roots in Minimalism, Drone, Noise, & Lo Fi. This was my original intent behind the contemplative sound, and music I created, whether I am successful, I leave that in your hands and your mind.', 
  9);
 
-   -- Insert record with ID 11
+-- Insert record with ID 11
 INSERT INTO albums 
 (id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
 VALUES 
 (11,
  '01040111',
- 'Inner Moments of Light', 
- '/static/images/Covers/10_INNER MOMENTS.png', 
- '2015-11-15', 
- '2022-07-09', 
+ 'Nocturnes & Reveries', 
+ '/static/images/Covers/11_NOCTURNES-REVERIES.png', 
+ '2022-08-23', 
+ '2023-09-03', 
  1,
- 'All Music & Sound: Roderick Shoolbraid Album Cover Design: Roderick Shoolbraid All Rights Reserved.',
- 'A collection or curation of assorted tracks from 2003-2015. With some source files going back as far as 1996-7. Organized finally in a little volume of what I like to think of as sacred or holy music, music or sound for meditation into specific spaces, or at least it is what it is for me. Everyone will have their own experience of the ambient medicinal sounds, finding itself roots in Minimalism, Drone, Noise, & Lo Fi. This was my original intent behind the contemplative sound, and music I created, whether I am successful, I leave that in your hands and your mind.', 
+ 'All Sound and Composition: Roderick Shoolbraid. Album Cover Design: Roderick Shoolbraid. Painting: "Nocturnes" - Roderick Shoolbraid. All Rights Reserved.',
+ 'A collection of 15 compositions, experiments and musings; all centred around night scenes, dreams, the mystical, and the subconscious. Using more challenging approach to scales and harmony, the album is influenced by Olivier Messiaen, György Ligeti, Gurdjieff, Sufi and Eastern Music.', 
+ 15);
+
+
+    -- Insert record with ID 12
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(12,
+ '01040112',
+ 'Prefabrications', 
+ '/static/images/Covers/12_PREFABRICATION.png', 
+ '2022-12-15', 
+ '2023-01-24', 
+ 1,
+ 'All Sound and Composition: Roderick Shoolbraid Album Cover Design: Roderick Shoolbraid All Rights Reserved.',
+ '"Prefabrication" is an album that builds on the concept of Music for Seven Structures, it uses one central melodic passage interpreted from 10 different viewpoints.' || 
+ 'The motif is modified based on multiples of 4 and pitch in octaves, creating a library of relative sounds. These sounds are arranged together in patterns and structures using a fixed tempo of 120 bpm throughout the albums 10 pieces.' ||
+ ' Predetermined edits and automation of effects are done to a grid to create an environment of chance. After this "prefabrication" stage, a more detailed finish is applied , drawing from the lexicon of modern electronica and IDM.' ||
+ 'The album is intended to be a collection of remixes on the subject of the experience of being inside a simple minimalist designed building.', 
+ 10);
+
+     -- Insert record with ID 13
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(13,
+ '01040113',
+ 'Vagary', 
+ '/static/images/Covers/13_VAGARY.png', 
+ '2023-01-15', 
+ '2023-02-11', 
+ 1,
+ 'All Sound and Composition: Roderick Shoolbraid Album Cover Design: Roderick Shoolbraid All Rights Reserved.',
+ 'A series of 8 tone poems on the inward experience of my healing journey.  Written over a course of 6 months from 2022, to the following year.', 
+ 8);
+
+      -- Insert record with ID 14
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(14,
+ '01040114',
+ 'Amsterdam Concreet', 
+ '/static/images/Covers/14_AMSTERDAM-CONCREET.png', 
+ '2023-02-15', 
+ '2023-02-20', 
+ 1,
+ 'All Sound and Composition: Roderick Shoolbraid Album Cover Design: Roderick Shoolbraid All Rights Reserved.',
+ '"Found Sound" and location recordings from Amsterdam, processed and layered. Compiled in collages and atmospheres, as a portal to introspective travel.', 
  9);
+
+       -- Insert record with ID 15
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(15,
+ '01040115',
+ 'Watercolours for Friends', 
+ '/static/images/Covers/15_WATERCOLOURS.png', 
+ '2023-03-20', 
+ '2023-03-24', 
+ 1,
+ 'All Sound and Composition: Roderick Shoolbraid Album Cover Design: Roderick Shoolbraid Watercolour: “Grass II” - Roderick Shoolbraid All Rights Reserved.',
+ 'This album was a response creatively to the large project and emotionally requiring: VAGARY. WATERCOLOURS FOR FRIENDS was a return to a more simple melodic structure and production values of my first releases. With that in mind, I used sound sources and instruments from my first few years at sound design collage from 2002-3, with a view to create a minimalist palette.
+A wonderful friend had remarked that though she appreciated the foreboding, emotional gravity and unsettling nature of VAGARY and other work, it was pieces like "Spherical" of NONAGON that she liked. As she could do other things and be in the space with the music.
+These comments greatly influenced the composition and structure of the sound, treating them like large rooms of watercolour paintings that one could be inside.', 
+ 7);
+
+        -- Insert record with ID 16
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(16,
+ '01040116',
+ 'Postcards from Old Sounds', 
+ '/static/images/Covers/16_POSTACARDS-OLD-SOUNDS.png', 
+ '2023-04-20', 
+ '2023-04-30', 
+ 1,
+ 'All Sound and Composition: Roderick Shoolbraid Album Cover Design: Roderick Shoolbraid Photography: Roderick Shoolbraid All Rights Reserved.',
+ '“Postcards from Old Sounds” is a continuation from “Watercolours For Friends” in its approach to harmony and sound design. Using a long form minimalist structure, the idea is: that these small sounds from the early 2000s are carried forward into the present palette and shaped with modern tools. Telling us stories of where they have been. It was written in tandem with "Watercolours.." in the spring of 2023.', 
+ 5);
+
+-- Insert record with ID 17
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(17,
+ '01040117',
+ 'Sakura', 
+ '/static/images/Covers/17_SAKURA.png', 
+ '2023-06-15', 
+ '2023-06-19', 
+ 1,
+ 'All Sound and Instruments: Roderick Shoolbraid. Album Cover Design: Roderick Shoolbraid. All Rights Reserved.',
+ $$One starts with no real intentions sometimes, and after awhile the idea formulates and direction emerges. Such was the case with "SAKURA". Written during April and May of this year, 2023, the materials gathered from improvised sessions were interludes between the walks along the seaside, where the blossoms fell from the trees.
+All of this reminded me of the visit I had to Japan for 6 months in 1998-1999. I had been listening to a lot of city pop over the winter, and thinking about my good late friend Peter Stufano. I decided to use all of this as a starting point.
+I had a small library of samples of my friends playing several instruments: tabla, dumbek, kalimba, and flute; and these became the sources in sampler instruments for the melodies. I used location recordings of the walks along the sea and record skips from the end of the city pop & Japanese jazz records I was listening to as a backdrop. The piano samples are from public outdoor instrument recorded a couple years ago, and the guitar work was completed this month.
+These are the materials used to create the sonic paintings of my memories, please enjoy & thank you for listening.
+-Roderick$$, 
+ 7);
+
+-- Insert record with ID 18
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(18,
+ '01040118',
+ 'Poem for a Homeworld', 
+ '/static/images/Covers/18_POEM-HOMEWORLD.png', 
+ '2023-10-20', 
+ '2023-10-25', 
+ 1,
+ 'All Sound and Instruments: Roderick Shoolbraid. Album Cover Design: Roderick Shoolbraid. All Rights Reserved.',
+ $$A cascade of experiences, a free fall into pulp science fiction of singular memory, set against 1990s electronic music’s landscape & language. What is the plot summary for our protagonist? Only the environments know. Written over 7 months pulling from reservoirs of old and new sounds: "Poem for a Homeworld" has a history of its own. Where are we in the index of portals? "Have I not been here before?" asks the lone traveller as all paths lead back to the beginning of the contemplations: "Am I alone? If I am alone in the universe, am I then not alone? If I am held in the embrace of the expanse of the universe?" These are the questions and revelations of our story, painted in the colours and reference of nostalgia.
+A lament from a distant future, a chaos of remembrance; an understanding that all time exists at once, on the cusp of the Void. A narrative set against the abstracted lexicon of 90s ambient drum & bass on a bed of watercolours of experiments. Completed by the fall of 2023.$$,
+ 15);
+
+ -- Insert record with ID 19
+INSERT INTO albums 
+(id, catalogue, name, cover_url, production_date, release_date, artist_id, credit, description, tracks) 
+VALUES 
+(19,
+ '01040119',
+ 'Rhombus', 
+ '/static/images/Covers/19_RHOMBUS.png', 
+ '2023-12-15', 
+ '2024-01-01', 
+ 1,
+ $$All Sound and Music: Roderick Shoolbraid
+Words: Roderick Shoolbraid -- except for "Art District" : Lovina Lakatua
+Vocal Samples All other tracks: Roderick Shoolbraid
+
+"Art District" :
+Vocal sample provided by: Lovina Lakatua
+
+"A window is a Friend of Rain" :
+Vocal sample provided by: Lovina & Ruby Lakatua
+
+Cover:
+Original Art: Kristin Shoolbraid
+Processing and Cover design: Roderick Shoolbraid$$,
+ $$Written in 2023, "Rhombus" was a brief break away from "Poem For a Homeworld". It revealed itself a collection of sounds distilled into larger ideas. I had been learning about and working with AI, and the following assortment of music reflects this exploration, and the deeper meaning behind it all. It is written in lamenting tone, similar to "Poem...", but its more immediate in its time scale and locale. A strange new future awaits us I am sure, these are the meditations on those themes.$$,
+ 15);
