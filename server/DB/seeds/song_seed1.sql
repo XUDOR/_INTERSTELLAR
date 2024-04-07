60374830486cb88575e02086591ac5f687d337f9
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-06 22:16:58 -0700

    Seeded albums #6-10

--- server/DB/seeds/song_seed1.sql
+++ server/DB/seeds/song_seed1.sql
@@ -51,4 +51,66 @@ VALUES
 ('0506', 'Flower', 'http://example.com/audio/song6.mp3', '00:03:45',1, 5, 6),
 ('0507', 'Streams', 'http://example.com/audio/song6.mp3', '00:05:29',1, 5, 7),
 ('0508', 'Tidal', 'http://example.com/audio/song6.mp3', '00:04:55',1, 5, 8),
-('0509', 'Waters', 'http://example.com/audio/song6.mp3', '00:05:46',1, 5, 9);
+('0509', 'Waters', 'http://example.com/audio/song6.mp3', '00:05:46',1, 5, 9),
+
+--Outer Corners--
+
+('0601', 'Stars', 'http://example.com/audio/song1.mp3', '00:06:23', 1, 6, 1),
+('0602', 'Galaxies', 'http://example.com/audio/song2.mp3', '00:05:49', 1, 6, 2),
+('0603', 'Dimensions', 'http://example.com/audio/song3.mp3', '00:02:25', 1, 6, 3),
+('0604', 'Nebulae', 'http://example.com/audio/song4.mp3', '00:04:54', 1, 6, 4),
+('0605', 'Galactic Cosmic Rays', 'http://example.com/audio/song5.mp3', '00:07:34', 1, 6, 5),
+('0606', 'Quantum', 'http://example.com/audio/song6.mp3', '00:05:40',1, 6, 6),
+('0607', 'Stellar', 'http://example.com/audio/song6.mp3', '00:05:38',1, 6, 7),
+
+--Nonagon--
+
+('0701', 'Spherical', 'http://example.com/audio/song1.mp3', '00:05:00', 1, 7, 1),
+('0702', 'Rhombus', 'http://example.com/audio/song2.mp3', '00:05:25', 1, 7, 2),
+('0703', 'Triangular', 'http://example.com/audio/song3.mp3', '00:03:58', 1, 7, 3),
+('0704', 'Curvature', 'http://example.com/audio/song4.mp3', '00:04:16', 1, 7, 4),
+('0705', 'Nonagon', 'http://example.com/audio/song5.mp3', '00:04:13', 1, 7, 5),
+('0706', 'Quadrants', 'http://example.com/audio/song6.mp3', '00:07:17',1, 7, 6),
+('0707', 'Dodecahedron', 'http://example.com/audio/song6.mp3', '00:06:05',1, 7, 7),
+('0708', 'Arc', 'http://example.com/audio/song6.mp3', '00:08:53',1, 7, 8),
+('0709', 'Vertices', 'http://example.com/audio/song6.mp3', '00:08:17',1, 7, 9),
+
+--Ambient Garden One--
+
+('0801', 'Stars | Outer Corners', 'http://example.com/audio/song1.mp3', '00:06:23', 1, 8, 1),
+('0802', 'Waters | Natura', 'http://example.com/audio/song2.mp3', '00:05:46', 1, 8, 2),
+('0803', 'Caterpillar | Glass City of Us', 'http://example.com/audio/song3.mp3', '00:06:43', 1, 8, 3),
+('0804', 'Flower | Natura', 'http://example.com/audio/song4.mp3', '00:03:45', 1, 8, 4),
+('0805', 'Beaches | New Domes of Earth', 'http://example.com/audio/song5.mp3', '00:03:58', 1, 8, 5),
+('0806', 'Perfumed Letter | Charlotta', 'http://example.com/audio/song6.mp3', '00:04:48',1, 8, 6),
+('0807', 'Stellar | Outer Corners', 'http://example.com/audio/song6.mp3', '00:05:38',1, 8, 7),
+('0808', 'Dawn for Children | New Domes of Earth', 'http://example.com/audio/song6.mp3', '00:04:29',1, 8, 8),
+('0809', 'Optical Record Pipe | Glass City of Us', 'http://example.com/audio/song6.mp3', '00:07:44',1, 8, 9),
+('0810', 'Nonagon | Nonagon', 'http://example.com/audio/song6.mp3', '00:04:13',1, 8, 10),     
+('0811', 'V | Objects & Particles', 'http://example.com/audio/song6.mp3', '00:06:54',1, 8, 11),     
+('0812', 'Arc | Nonagon', 'http://example.com/audio/song6.mp3', '00:08:53',1, 8, 12),     
+('0813', 'Jade Flower Memory Index | Objects & Particles', 'http://example.com/audio/song6.mp3', '00:06:14',1, 8, 13),          
+('0814', 'Charlotta | Charlotta', 'http://example.com/audio/song6.mp3', '00:08:19',1, 8, 14),   
+
+--Music for Seven Structures--
+
+('0901', 'I', 'http://example.com/audio/song1.mp3', '00:06:51', 1, 9, 1),
+('0902', 'II', 'http://example.com/audio/song2.mp3', '00:04:32', 1, 9, 2),
+('0903', 'III', 'http://example.com/audio/song3.mp3', '00:04:47', 1, 9, 3),
+('0904', 'IV', 'http://example.com/audio/song4.mp3', '00:02:41', 1, 9, 4),
+('0905', 'V', 'http://example.com/audio/song5.mp3', '00:05:39', 1, 9, 5),
+('0906', 'VI', 'http://example.com/audio/song6.mp3', '00:06:53',1, 9, 6),
+('0907', 'VII', 'http://example.com/audio/song6.mp3', '00:06:58',1, 9, 7),
+
+
+--Inner Moments of Light--
+
+('1001', 'Moment of Light', 'http://example.com/audio/song1.mp3', '00:04:34', 1, 10, 1),
+('1002', 'Landscape II', 'http://example.com/audio/song2.mp3', '00:03:00', 1,10, 2),
+('1003', 'Prisms', 'http://example.com/audio/song3.mp3', '00:08:08', 1, 10, 3),
+('1004', 'Sphere', 'http://example.com/audio/song4.mp3', '00:12:44', 1, 10, 4),
+('1005', 'Beauty of the Heart', 'http://example.com/audio/song5.mp3', '00:04:13', 1, 10, 5),
+('1006', 'Beginnings', 'http://example.com/audio/song6.mp3', '00:06:53',1, 10, 6),
+('1007', 'Landscape I', 'http://example.com/audio/song6.mp3', '00:03:19',1, 10, 7),
+('1008', 'Universal Knowledge', 'http://example.com/audio/song6.mp3', '00:04:25',1, 10, 8),
+('1009', 'Love is the Universe', 'http://example.com/audio/song6.mp3', '00:04:27',1, 10, 9),
