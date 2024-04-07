79a9a3739ba49b801e60bc36c31b91d7b22d68d9
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-07 12:31:28 -0700

    removal of container name... as design refactor continues

--- client/src/components/Content/Album/SongList.jsx
+++ client/src/components/Content/Album/SongList.jsx
@@ -5,7 +5,6 @@ import Song from './Song';
 function SongList({ songs }) {
   return (
     <div className='SongList'>
-      songlist
       {songs.map((song) => (
         <Song key={song.id} song={song} />
       ))}
