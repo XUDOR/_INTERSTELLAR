39b5e36b097266073349b8234c9e622d7ce91f6b
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-07 16:33:13 -0700

    jsx div import and placement

--- client/src/components/Content/Album/InfoContainer.jsx
+++ client/src/components/Content/Album/InfoContainer.jsx
@@ -1,11 +1,16 @@
 // infocontainer.jsx
 import React from 'react';
+import Catalogue from './Catalogue';
+import Description from './Description';
+import Credit from './Credit';
 import './InfoContainer.css';
 
 function InfoContainer() {
   return (
     <div className='InfoContainer'>
-      Information
+    <Catalogue />
+    <Description />
+    <Credit />
     </div>
   );
 }
