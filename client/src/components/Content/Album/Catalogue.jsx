7c7df7bffd26d239f9c70a22763e8c46e814efd3
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-07 22:03:16 -0700

    removed console log

--- client/src/components/Content/Album/Catalogue.jsx
+++ client/src/components/Content/Album/Catalogue.jsx
@@ -1,15 +1,13 @@
-// Catalogue.jsx
+// Catalogue.js
 import React from 'react';
 import './Catalogue.css';
 
 function Catalogue({ catalogue }) {
-  console.log('Catalogue:', catalogue); // Log the catalogue prop
   return (
     <div className='CatalogueContent'>
-      {catalogue} catalogue
+      {catalogue} 
     </div>
   );
 }
-
 
 export default Catalogue;
