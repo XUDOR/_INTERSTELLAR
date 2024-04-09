5aa579541e43e32af2202a5e54ea08b77f7e1da2
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-08 22:41:52 -0700

    removing tabLIST

--- client/src/components/MainSection/Main.jsx
+++ client/src/components/MainSection/Main.jsx
@@ -1,48 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useMusicData } from '../../Contexts/MusicDataContext';
-import GalleryDisplay from '../Gallery/GalleryDisplay';
-import TabList from './TabList';
-import Current from '../Current/Current';
-import InfoContainer from '../Content/Album/InfoContainer';
+import Cover from '../Content/Album/Cover'; // Corrected import path
+import AlbumTitle from '../Content/Album/AlbumTitle'; // Adjust the import path as necessary
+
+
+
 import './Main.css';
 
 function Main() {
-  const { albumIndex } = useMusicData();
-  const [activeTab, setActiveTab] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const { state, dispatch } = useMusicData();
 
-  // Log the state of albumIndex on each render
-  console.log("Main component render, albumIndex:", albumIndex);
-
-  useEffect(() => {
-    console.log("Main useEffect for albumIndex:", albumIndex);
-    if (Object.keys(albumIndex).length > 0) {
-      const firstAlbumId = Object.keys(albumIndex)[0];
-      console.log("Setting activeTab to the first albumId:", firstAlbumId);
-      setActiveTab(firstAlbumId); // Default to the first album
-      setIsLoading(false);
-    }
-  }, [albumIndex]);
-
-  const tabLabels = Object.values(albumIndex).map(({ id, name }) => ({ id, name }));
-
-  const handleTabClick = (albumId) => {
-    console.log("Tab clicked, setting activeTab:", albumId);
-    setActiveTab(albumId);
+  // Function to handle clicking on an album div
+  const handleAlbumClick = (albumId) => {
+    // Dispatch to set an active album ID
+    dispatch({ type: 'SET_ACTIVE_TAB', payload: albumId });
   };
 
-  if (isLoading) {
-    console.log("Main component displaying loading state.");
-    return <div>Loading...</div>;
-  }
+  // Function to render minimal album information as clickable divs
+  const renderAlbumDivs = () => {
+    return Object.entries(state.albumIndex).map(([albumId, { name, cover_url }]) => (
 
-  console.log("Main component rendering content, activeTab:", activeTab);
+
+      <div key={albumId} className="album-div" onClick={() => handleAlbumClick(albumId)}>
+        <div className="album-info">
+          <h3 className='albumTitle'>{name}</h3>
+          {/* You can expand here to include more information if needed */}
+        </div>
+        <Cover imageUrl={cover_url} altText={`Cover of ${name}`} />
+        
+      </div>
+    ));
+  };
+
   return (
     <div className='Main'>
-      <TabList activeTab={activeTab} onTabClick={handleTabClick} labels={tabLabels} />
-      <GalleryDisplay activeTab={activeTab} />
-      <InfoContainer />
-      <Current />
+      {state.isLoading ? <div>Loading...</div> : renderAlbumDivs()}
     </div>
   );
 }
