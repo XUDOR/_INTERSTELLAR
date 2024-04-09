6815eb4cf45b42f8b0fe76ab792df6ec300d6db0
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-08 22:42:49 -0700

    using usereducer for the logic

--- client/src/Contexts/MusicDataContext.js
+++ client/src/Contexts/MusicDataContext.js
@@ -1,77 +1,13 @@
-// MusicDataContext.js
-import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
+import React, { createContext, useContext } from 'react';
+import { useMusicDataReducer } from '../hooks/useMusicDataReducer';
 
 export const MusicDataContext = createContext();
 
 export const MusicDataProvider = ({ children }) => {
-  const [albumIndex, setAlbumIndex] = useState({});
-  const [songIndex, setSongIndex] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const [activeTab, setActiveTab] = useState(null);
+  const [state, dispatch] = useMusicDataReducer();
 
-  const fetchData = useCallback(async () => {
-    console.log("Fetching data...");
-    setIsLoading(true);
-    try {
-      const albumResponse = await fetch('http://localhost:5001/api/albums');
-      const songResponse = await fetch('http://localhost:5001/api/songs');
-      console.log("Responses received");
-      if (!albumResponse.ok || !songResponse.ok) throw new Error('Failed to fetch data');
-      const albums = await albumResponse.json();
-      const songs = await songResponse.json();
-      console.log("Data parsed to JSON", { albums, songs });
-      
-      const transformedAlbums = albums.reduce((acc, album) => {
-        acc[album.id] = {
-          ...album,
-          catalogueData: {
-            catalogue: album.catalogue,
-            description: album.description,
-            credit: album.credit,
-          },
-        };
-        return acc;
-      }, {});
-      
-      setAlbumIndex(transformedAlbums);
-      setSongIndex(songs.reduce((acc, song) => ({ ...acc, [song.id]: song }), {}));
-      setActiveTab(Object.keys(albums)[0]); // Set default activeTab to the first album
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    } finally {
-      setIsLoading(false);
-      console.log("Fetching complete");
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
-  useEffect(() => {
-    console.log("Album index updated:", albumIndex);
-  }, [albumIndex]);
-  
-  useEffect(() => {
-    console.log("Song index updated:", songIndex);
-  }, [songIndex]);
-  
-  useEffect(() => {
-    console.log("Loading state:", isLoading);
-  }, [isLoading]);
-  
-  useEffect(() => {
-    console.log("Active tab set to:", activeTab);
-  }, [activeTab]);
-  
   return (
-    <MusicDataContext.Provider value={{
-      albumIndex, 
-      songIndex, 
-      isLoading, 
-      activeTab, 
-      setActiveTab,
-    }}>
+    <MusicDataContext.Provider value={{ state, dispatch }}>
       {children}
     </MusicDataContext.Provider>
   );
