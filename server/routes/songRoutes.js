a1ec14c78154fb76e9d88f8e5e7b983fb2609030
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-09 20:05:57 -0700

    updated route for songs by albumID

--- server/routes/songRoutes.js
+++ server/routes/songRoutes.js
@@ -1,9 +1,9 @@
 // songRoutes.js
-
 const express = require('express');
 const router = express.Router();
 
-const { getSongById, getAllSongs } = require('../services/songService');
+// Assuming you have or will create this function in your song service
+const { getSongById, getAllSongs, getSongsByAlbumId } = require('../services/songService');
 
 // Fetch all songs
 router.get('/', async (req, res) => {
@@ -18,15 +18,34 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Fetch songs by album ID
+router.get('/album/:albumId', async (req, res) => {
+  const { albumId } = req.params;
+  console.log(`Received request to fetch songs for album ID: ${albumId}`);
+  try {
+    const songs = await getSongsByAlbumId(albumId);
+    if (songs.length > 0) {
+      console.log(`Fetched songs for album ID: ${albumId}`, songs);
+      res.json(songs);
+    } else {
+      console.log(`No songs found for album ID: ${albumId}`);
+      res.status(404).send('No songs found for this album');
+    }
+  } catch (err) {
+    console.error(`Error fetching songs for album ID: ${albumId}`, err);
+    res.status(500).send('Server error');
+  }
+});
+
 // Fetch a song by ID
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   console.log(`Received request to fetch song with ID: ${id}`);
   try {
     const song = await getSongById(id);
-    if (song.length > 0) {
-      console.log('Fetched song:', song[0]);
-      res.json(song[0]);
+    if (song) {
+      console.log('Fetched song:', song);
+      res.json(song);
     } else {
       console.log(`Song with ID: ${id} not found`);
       res.status(404).send('Song not found');
@@ -38,3 +57,4 @@ router.get('/:id', async (req, res) => {
 });
 
 module.exports = router;
+
