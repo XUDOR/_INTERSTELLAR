66504f5ccf85ddb0e785633334ac1a6c043584a5
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-09 17:54:05 -0700

    refactor

--- client/src/components/MainSection/Main.jsx
+++ client/src/components/MainSection/Main.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import { useMusicData } from '../../Contexts/MusicDataContext';
-import Cover from '../Content/Album/Cover'; // Corrected import path
-//import AlbumTitle from '../Content/Album/AlbumTitle'; // Adjust the import path as necessary
-
-
-
+import Cover from '../Content/Album/Cover';
+import InfoContainer from '../Content/Album/InfoContainer';
+import SongList from '../Content/Album/SongList';
 import './Main.css';
+import '../Content/Album/Cover.css';
+import '../Content/Album/InfoContainer.css';
 
 function Main() {
-  const { state, dispatch } = useMusicData();
+  const { state } = useMusicData();
 
-  // Function to handle clicking on an album div
-  const handleAlbumClick = (albumId) => {
-    // Dispatch to set an active album ID
-    dispatch({ type: 'SET_ACTIVE_TAB', payload: albumId });
-  };
+  console.log("songIndex state at Main component:", state.songIndex); // Log the entire songIndex state for debugging
 
-  // Function to render minimal album information as clickable divs
   const renderAlbumDivs = () => {
-    return Object.entries(state.albumIndex).map(([albumId, { name, cover_url }]) => (
+    return Object.entries(state.albumIndex).map(([albumId, { name, cover_url }]) => {
 
+      // Place debugging logs here to check the structure, keys, and values
+      console.log("Keys in songIndex:", Object.keys(state.songIndex).map(key => typeof key));
+      console.log("Type and value of albumId being accessed:", typeof albumId, albumId);
+      console.log(state.songIndex); // Check the structure and contents
+      console.log(albumId); // Ensure this matches keys in songIndex
 
-      <div key={albumId} className="album-div" onClick={() => handleAlbumClick(albumId)}>
-        <div className="album-info">
-          <h3 className='albumTitle'>{name}</h3>
-          {/* You can expand here to include more information if needed */}
+      // Attempt to retrieve songs for this specific albumId from songIndex
+      const songsForThisAlbum = state.songIndex[albumId] || [];
+
+      console.log("Mapping albumId to songs", {
+        albumId,
+        songsForThisAlbum: songsForThisAlbum
+      });
+
+      return (
+        <div key={albumId} className="album-container">
+          <div className="album-info">
+            <h3 className='albumTitle'>{name}</h3>
+            <Cover imageUrl={cover_url} altText={`Cover of ${name}`} />
+          </div>
+          <SongList songs={songsForThisAlbum} />
+          <InfoContainer albumId={albumId} />
         </div>
-        <Cover imageUrl={cover_url} altText={`Cover of ${name}`} />
-        
-      </div>
-    ));
+      );
+    });
   };
 
   return (
