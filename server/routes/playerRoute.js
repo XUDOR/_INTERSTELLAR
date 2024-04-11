98048f85b4e354e6e8f7474313682c5c222c9cba
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-10 17:41:51 -0700

    route

---
+++ server/routes/playerRoute.js
@@ -0,0 +1,9 @@
+// playerRoutes.js
+
+const express = require('express');
+const router = express.Router();
+
+// Here you will later add route handlers, like:
+// router.get('/', (req, res) => res.send('User route placeholder'));
+
+module.exports = router;
