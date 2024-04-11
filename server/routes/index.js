9115c76ac1440d15e55bd6e7f06582694d9a744a
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-10 17:41:40 -0700

    playerRoute created

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,3 @@
-///routes/index.js
-
 const express = require('express');
 const router = express.Router();
 
@@ -8,13 +6,15 @@ const adminRoutes = require('./adminRoutes');
 //const artistRoutes = require('./artistRoutes');
 const songRoutes = require('./songRoutes');
 const coverRoutes = require('./coverRoutes');
+//const playerRoutes = require('./playerRoutes'); 
 // Import other routes here
 
 router.use('/albums', albumRoutes);
 router.use('/admin', adminRoutes);
 //router.use('/artists', artistRoutes);
 router.use('/songs', songRoutes);
-router.use('/covers',coverRoutes);
+router.use('/covers', coverRoutes);
+//router.use('/player', playerRoutes);
 // Use other routes here
 
 module.exports = router;
