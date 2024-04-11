4cb3573b5ad4d055965f4aa8bbc34ca7532f4f09
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-10 17:40:18 -0700

    props

--- client/src/components/Footer/Player.jsx
+++ client/src/components/Footer/Player.jsx
@@ -1,11 +1,42 @@
 //Footer/Player
 import React from 'react';
+import AudioPlayer from '../audioPlayer/AudioPlayer';
 import './Player.css';
 
-const Player = () => {
+
+const Player = (props) => {
+  const {
+    audioFileName,
+    handleLoadedData,
+    handleTimeUpdate, setIsPlaying, handleSeekMouseDown,
+    handleSeekMouseUp, calculateTime, currentTime, duration,
+    handleSeekChange, handleVolumeChange, togglePlayPause,
+    isPlaying, isSeeking, volume, audioPlayer } = props
+
+
+
   return (
     <div className="Player">
-      Player
+      <AudioPlayer
+        audioFileName={audioFileName}
+        handleLoadedData={handleLoadedData}
+        handleTimeUpdate={handleTimeUpdate}
+        setIsPlaying={setIsPlaying}
+        handleSeekMouseDown={handleSeekMouseDown}
+        handleSeekMouseUp={handleSeekMouseUp}
+        handleSeekChange={handleSeekChange}
+        handleVolumeChange={handleVolumeChange}
+        volume={volume}
+        duration={duration}
+        togglePlayPause={togglePlayPause}
+        audioPlayer={audioPlayer}
+        calculateTime={calculateTime}
+        currentTime={currentTime}
+        isPlaying={isPlaying}
+        isSeeking={isSeeking}
+        
+
+      />
     </div>
   );
 };
