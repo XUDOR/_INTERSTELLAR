84b8b5ba8139ddacff5f5ce22172021eae063c47
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-10 17:40:08 -0700

    props being passed

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -9,14 +9,42 @@ import User from './User';
 import Downloads from './Downloads';
 import './Footer.css';
 
-const Footer = () => {
+const Footer = (props) => {
+  const {
+    audioFileName,
+    handleLoadedData,
+    handleTimeUpdate, setIsPlaying, handleSeekMouseDown, 
+    handleSeekMouseUp, calculateTime, currentTime, duration, 
+    handleSeekChange, handleVolumeChange, togglePlayPause, 
+    isPlaying,isSeeking,volume, audioPlayer  } = props
+
+
+
   return (
     <footer className="Footer">
       <Admin />
       <Downloads />
       <User />
       <Nav />
-      <Player />
+      <Player 
+         audioFileName={audioFileName}
+         handleLoadedData={handleLoadedData}
+         handleTimeUpdate={handleTimeUpdate}
+         setIsPlaying={setIsPlaying}
+         handleSeekMouseDown={handleSeekMouseDown}
+         handleSeekMouseUp={handleSeekMouseUp}
+         handleSeekChange={handleSeekChange}
+         handleVolumeChange={handleVolumeChange}
+         volume={volume}  
+         duration={duration} 
+         togglePlayPause={togglePlayPause}
+         audioPlayer={audioPlayer}
+         calculateTime={calculateTime}
+         currentTime={currentTime}
+         isPlaying={isPlaying}
+         isSeeking={isSeeking}
+         
+      />
       <Radio />
       <Shop />
     </footer>
