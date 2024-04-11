58abf346d81444a9b25afc760a0f12629f54f9cf
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-10 17:39:52 -0700

    props being passed to songlist

--- client/src/components/Content/Album/SongList.jsx
+++ client/src/components/Content/Album/SongList.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 import Song from './Song';
 import './SongList.css';
 
-function SongList({ songs }) {
+function SongList({ songs, setaudioFIleName }) {
   console.log("Received songs in SongList:", songs);
   return (
     <div className='SongList'>
       
       {/* Check if songs is defined and is an array */}
       {Array.isArray(songs) && songs.map((song) => (
-        <Song key={song.id} song={song} />
+        <Song key={song.id} song={song}
+        setaudioFIleName={setaudioFIleName} />
       ))}
     </div>
   );
