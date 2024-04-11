7f753f63d8753d089f90b5588226cea070430975
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-10 17:38:38 -0700

    remove logic to top level

--- client/src/components/audioPlayer/AudioPlayer.jsx
+++ client/src/components/audioPlayer/AudioPlayer.jsx
@@ -1,82 +1,22 @@
-import React, { useState, useRef } from "react";
-import audioFile from '../../Audio-Assets/CHARLOTTA.mp3';
-import '../../App.css';
+import React from "react";
+//import audioFile from '../../Audio-Assets/CHARLOTTA.mp3';
+import './AudioPlayer.css';
 
-const AudioPlayer = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [duration, setDuration] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
-  const [volume, setVolume] = useState(1.0);
-  const [isSeeking, setIsSeeking] = useState(false);
-
-  const audioPlayer = useRef(); // Reference to the audio component
-
-  // File name for display. If the audio file path is static, this can remain constant.
-  const audioFileName = "CHARLOTTA.mp3"; // Directly using the file name since the path is static
-
-  // Toggles the play/pause state
-  const togglePlayPause = () => {
-    const prevValue = isPlaying;
-    setIsPlaying(!prevValue);
-    if (!prevValue) {
-      audioPlayer.current.play();
-    } else {
-      audioPlayer.current.pause();
-    }
-  };
-
-  // Sets the duration once audio metadata is loaded
-  const handleLoadedData = () => {
-    setDuration(audioPlayer.current.duration);
-    if (isPlaying) audioPlayer.current.play();
-  };
-
-  // Updates the current time as the audio plays
-  const handleTimeUpdate = () => {
-    if (!isSeeking) {
-      setCurrentTime(audioPlayer.current.currentTime);
-    }
-  };
-
-  // Handles the change in volume
-  const handleVolumeChange = (e) => {
-    const newVolume = e.target.value;
-    audioPlayer.current.volume = newVolume;
-    setVolume(newVolume);
-  };
-
-  // Handles the change in the seek bar
-  const handleSeekChange = (e) => {
-    const newTime = e.target.value;
-    audioPlayer.current.currentTime = newTime;
-    setCurrentTime(newTime);
-  };
-
-  // Indicates the user has started dragging the seek bar
-  const handleSeekMouseDown = () => {
-    setIsSeeking(true);
-  };
-
-  // Indicates the user has finished dragging the seek bar
-  const handleSeekMouseUp = () => {
-    setIsSeeking(false);
-    audioPlayer.current.currentTime = currentTime;
-  };
-
-  // Converts the time in seconds to a displayable format
-  const calculateTime = (secs) => {
-    const minutes = Math.floor(secs / 60);
-    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    const seconds = Math.floor(secs % 60);
-    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-    return `${returnedMinutes}:${returnedSeconds}`;
-  };
+const AudioPlayer = (props) => {
+  const {
+    audioFileName,
+    handleLoadedData,
+    handleTimeUpdate, setIsPlaying, handleSeekMouseDown, 
+    handleSeekMouseUp, calculateTime, currentTime, duration, 
+    handleSeekChange, handleVolumeChange, togglePlayPause, 
+    isPlaying,isSeeking,volume, audioPlayer  } = props
+  
 
   return (
     <div className="player">
       <audio
         ref={audioPlayer}
-        src={audioFile}
+        src={audioFileName}
         onLoadedData={handleLoadedData}
         onTimeUpdate={handleTimeUpdate}
         onEnded={() => setIsPlaying(false)}
