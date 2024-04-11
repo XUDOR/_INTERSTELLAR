22e26313a885dda52e957c37d3bd8a5d2c5abc44
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-10 17:39:33 -0700

    icons added for toPLay to player

--- client/src/components/Content/Album/Song.jsx
+++ client/src/components/Content/Album/Song.jsx
@@ -1,16 +1,29 @@
 // Song.jsx
 import React from 'react';
 import './Song.css'
+import play from "../../../images/Icons/Play.png"
 
 // Adjusting the Song component to match your data structure
-function Song({ song }) {
+function Song({ song, setaudioFIleName }) {
   console.log("Rendering song:", song);
+
+const handleClickPlay = (songToPlay) => {
+  console.log("PLay-clicked",songToPlay);
+setaudioFIleName(songToPlay.audio_url)
+}
+
+
+
   return (
     <div className='Song'>
       
       {/* Assuming `name` is the correct property based on your state log */}
       <div>{song.name}</div>
       {/* Display more song details here */}
+    
+      <div onClick={()=>handleClickPlay(song)}>
+        <img src={play} alt="play" />  
+        </div>
     </div>
   );
 }
