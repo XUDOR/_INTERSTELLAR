2232e4c68ddf457d311b55719066a6a83ed94c13
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-11 19:20:51 -0700

    passing props

--- client/src/components/Content/Album/Song.jsx
+++ client/src/components/Content/Album/Song.jsx
@@ -1,23 +1,19 @@
-// Song.jsx
 import React from 'react';
 import './Song.css';
 
-function Song({ song, setaudioFIleName }) {
-  console.log("Rendering song:", song);
-
+function Song({ song, setCurrentSong }) {
   const handleClickPlay = (e) => {
-    console.log("Play clicked", song);
-    setaudioFIleName(song.audio_url);
-    e.preventDefault(); // If needed to prevent any default behavior
+    e.preventDefault(); // Prevent default action (important if this is in a form or link)
+    console.log("Playing song:", song);
+    setCurrentSong(song); // Setting the current song object
   };
 
-  // Adding accessibility enhancements
   return (
     <div className='Song' onClick={handleClickPlay} role="button" tabIndex="0">
       <div>{song.name}</div>
-      {/* You can add more song details here if needed */}
     </div>
   );
 }
+
 
 export default Song;
