cec1bdb4e161c28a35d6606eed70206896d0095b
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-11 19:22:47 -0700

    update to footer for the new props from main(for audioPlayer)

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,90 @@
-// Footer.jsx, located at src/components/Footer/Footer.jsx
+// Footer.jsx
+import React from 'react';
+import Player from './Player';
+import Radio from './Radio';
+import Shop from './Shop';
+import Nav from './Nav';
+import Admin from './Admin';
+import User from './User';
+import Downloads from './Downloads';
+import './Footer.css';
+
+const Footer = (props) => {
+  const {
+    currentSong, // Use only this for audio details
+    handleLoadedData,
+    handleTimeUpdate,
+    setIsPlaying,
+    handleSeekMouseDown,
+    handleSeekMouseUp,
+    calculateTime,
+    currentTime,
+    duration,
+    handleSeekChange,
+    handleVolumeChange,
+    togglePlayPause,
+    isPlaying,
+    isSeeking,
+    volume,
+    audioPlayer
+  } = props;
+
+  return (
+    <footer className="Footer">
+      <Admin />
+      <Downloads />
+      <User />
+      <Nav />
+      <Player
+        currentSong={currentSong}
+        handleLoadedData={handleLoadedData}
+        handleTimeUpdate={handleTimeUpdate}
+        setIsPlaying={setIsPlaying}
+        handleSeekMouseDown={handleSeekMouseDown}
+        handleSeekMouseUp={handleSeekMouseUp}
+        handleSeekChange={handleSeekChange}
+        handleVolumeChange={handleVolumeChange}
+        volume={volume}
+        duration={duration}
+        togglePlayPause={togglePlayPause}
+        audioPlayer={audioPlayer}
+        calculateTime={calculateTime}
+        currentTime={currentTime}
+        isPlaying={isPlaying}
+        isSeeking={isSeeking}
+      />
+      <Radio />
+      <Shop />
+    </footer>
+  );
+};
+
+export default Footer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Footer.jsx, located at src/components/Footer/Footer.jsx
 import React from 'react';
 import Player from './Player';
 import Radio from './Radio';
@@ -12,6 +98,7 @@ import './Footer.css';
 const Footer = (props) => {
   const {
     audioFileName,
+    currentSong,
     handleLoadedData,
     handleTimeUpdate, setIsPlaying, handleSeekMouseDown, 
     handleSeekMouseUp, calculateTime, currentTime, duration, 
@@ -28,6 +115,7 @@ const Footer = (props) => {
       <Nav />
       <Player 
          audioFileName={audioFileName}
+         currentSong={currentSong}
          handleLoadedData={handleLoadedData}
          handleTimeUpdate={handleTimeUpdate}
          setIsPlaying={setIsPlaying}
@@ -51,4 +139,4 @@ const Footer = (props) => {
   );
 };
 
-export default Footer;
+export default Footer; */ 
