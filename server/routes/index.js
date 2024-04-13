5e482988c0bfedc4fb071bd38487e463f84cc94d
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-13 12:56:59 -0700

    //commented out defunct routes.. may delete or use

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,18 +4,18 @@ const express = require('express');
 const router = express.Router();
 
 const albumRoutes = require('./albumRoutes');
-const adminRoutes = require('./adminRoutes');
+//const adminRoutes = require('./adminRoutes');
 //const artistRoutes = require('./artistRoutes');
 const songRoutes = require('./songRoutes');
-const coverRoutes = require('./coverRoutes');
+//const coverRoutes = require('./coverRoutes');
 //const playerRoutes = require('./playerRoutes'); 
 // Import other routes here
 
 router.use('/albums', albumRoutes);
-router.use('/admin', adminRoutes);
+//router.use('/admin', adminRoutes);
 //router.use('/artists', artistRoutes);
 router.use('/songs', songRoutes);
-router.use('/covers', coverRoutes);
+//router.use('/covers', coverRoutes);
 //router.use('/player', playerRoutes);
 // Use other routes here
 
