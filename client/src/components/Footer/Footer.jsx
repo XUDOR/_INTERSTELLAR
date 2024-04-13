a502b56fd003d5852447114d1e29912c13995b5e
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-12 21:13:11 -0700

    placing footer and queuein app for Sticky footer style

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-// Footer.jsx
 import React from 'react';
 import Player from './Player';
 import Queue from './Queue';
@@ -11,7 +10,7 @@ import './Footer.css';
 
 const Footer = (props) => {
   const {
-    currentSong, // Use only this for audio details
+    currentSong,
     handleLoadedData,
     handleTimeUpdate,
     setIsPlaying,
@@ -30,113 +29,35 @@ const Footer = (props) => {
   } = props;
 
   return (
-    <footer className="Footer">
-      <Admin />
-      <Downloads />
-      <User />
-      <Nav />
-      <Player
-        currentSong={currentSong}
-        handleLoadedData={handleLoadedData}
-        handleTimeUpdate={handleTimeUpdate}
-        setIsPlaying={setIsPlaying}
-        handleSeekMouseDown={handleSeekMouseDown}
-        handleSeekMouseUp={handleSeekMouseUp}
-        handleSeekChange={handleSeekChange}
-        handleVolumeChange={handleVolumeChange}
-        volume={volume}
-        duration={duration}
-        togglePlayPause={togglePlayPause}
-        audioPlayer={audioPlayer}
-        calculateTime={calculateTime}
-        currentTime={currentTime}
-        isPlaying={isPlaying}
-        isSeeking={isSeeking}
-      />
+    <div className="AppContainer">
+      <footer className="Footer">
+        <Admin />
+        <Downloads />
+        <User />
+        <Nav />
+        <Player
+          currentSong={currentSong}
+          handleLoadedData={handleLoadedData}
+          handleTimeUpdate={handleTimeUpdate}
+          setIsPlaying={setIsPlaying}
+          handleSeekMouseDown={handleSeekMouseDown}
+          handleSeekMouseUp={handleSeekMouseUp}
+          handleSeekChange={handleSeekChange}
+          handleVolumeChange={handleVolumeChange}
+          volume={volume}
+          duration={duration}
+          togglePlayPause={togglePlayPause}
+          audioPlayer={audioPlayer}
+          calculateTime={calculateTime}
+          currentTime={currentTime}
+          isPlaying={isPlaying}
+          isSeeking={isSeeking}
+        />
+        <Shop />
+      </footer>
       <Queue />
-      <Shop />
-    </footer>
+    </div>
   );
 };
 
 export default Footer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Footer.jsx, located at src/components/Footer/Footer.jsx
-import React from 'react';
-import Player from './Player';
-import Radio from './Radio';
-import Shop from './Shop';
-import Nav from './Nav';
-import Admin from './Admin';
-import User from './User';
-import Downloads from './Downloads';
-import './Footer.css';
-
-const Footer = (props) => {
-  const {
-    audioFileName,
-    currentSong,
-    handleLoadedData,
-    handleTimeUpdate, setIsPlaying, handleSeekMouseDown, 
-    handleSeekMouseUp, calculateTime, currentTime, duration, 
-    handleSeekChange, handleVolumeChange, togglePlayPause, 
-    isPlaying,isSeeking,volume, audioPlayer  } = props
-
-
-
-  return (
-    <footer className="Footer">
-      <Admin />
-      <Downloads />
-      <User />
-      <Nav />
-      <Player 
-         audioFileName={audioFileName}
-         currentSong={currentSong}
-         handleLoadedData={handleLoadedData}
-         handleTimeUpdate={handleTimeUpdate}
-         setIsPlaying={setIsPlaying}
-         handleSeekMouseDown={handleSeekMouseDown}
-         handleSeekMouseUp={handleSeekMouseUp}
-         handleSeekChange={handleSeekChange}
-         handleVolumeChange={handleVolumeChange}
-         volume={volume}  
-         duration={duration} 
-         togglePlayPause={togglePlayPause}
-         audioPlayer={audioPlayer}
-         calculateTime={calculateTime}
-         currentTime={currentTime}
-         isPlaying={isPlaying}
-         isSeeking={isSeeking}
-         
-      />
-      <Radio />
-      <Shop />
-    </footer>
-  );
-};
-
-export default Footer; */ 
