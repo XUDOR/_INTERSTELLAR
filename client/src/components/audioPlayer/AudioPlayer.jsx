9e7f7ecb1cb8be2db053f38b6d9ba3ec886295b0
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-12 20:03:42 -0700

    bringing in svg

--- client/src/components/audioPlayer/AudioPlayer.jsx
+++ client/src/components/audioPlayer/AudioPlayer.jsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useRef } from "react";
+import { ReactComponent as PlayIcon } from '../../images/Icons/play-blue.svg';
+import { ReactComponent as PauseIcon } from '../../images/Icons/Pause.svg'; // Note the capital 'P'
+import { ReactComponent as NextIcon } from '../../images/Icons/next.svg';
+import { ReactComponent as PreviousIcon } from '../../images/Icons/back.svg';
+
 import './AudioPlayer.css';
 
 const AudioPlayer = (props) => {
@@ -66,48 +71,55 @@ const AudioPlayer = (props) => {
 
   return (
     <div className="player">
-      <audio
-        ref={audioPlayer}
-        src={currentSong ? currentSong.audio_url : ''}
-        onLoadedData={handleLoadedData}
-        onTimeUpdate={handleTimeUpdate}
-      ></audio>
-      <div className="audio-file-name">
-        {currentSong ? currentSong.name : "No song loaded"}
-      </div>
-      <button className="Previous" onClick={playPreviousSong}>prev</button>
-      <button className="Play" onClick={togglePlayPause}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <button className="Next" onClick={playNextSong}>Next</button>
-      <div className="Time">
-        {calculateTime(currentTime)} / {calculateTime(duration)}
-      </div>
-      <div className="seek-control">
-        <input
-          type="range"
-          min="0"
-          max={duration || 0}
-          value={isSeeking ? undefined : currentTime}
-          onChange={handleSeekChange}
-          onMouseDown={handleSeekMouseDown}
-          onMouseUp={handleSeekMouseUp}
-          className="seek-slider"
-          step="1"
-        />
-      </div>
-      <div className="volume-control">
-        <input
-          type="range"
-          min="0"
-          max="1"
-          step="0.01"
-          value={volume}
-          onChange={handleVolumeChange}
-          className="volume-slider"
-        />
-      </div>
-    </div>
+  <audio
+    ref={audioPlayer}
+    src={currentSong ? currentSong.audio_url : ''}
+    onLoadedData={handleLoadedData}
+    onTimeUpdate={handleTimeUpdate}
+  ></audio>
+  <div className="audio-file-name">
+    {currentSong ? currentSong.name : "No song loaded"}
+  </div>
+  <button className="Back" onClick={playPreviousSong}>
+    <PreviousIcon className="svg-icon" />
+  </button>
+  <button className="Play" onClick={togglePlayPause}>
+    {isPlaying ? <PauseIcon className="svg-icon" />  : <PlayIcon className="svg-icon" />  
+
+}
+  </button>
+  <button className="Next" onClick={playNextSong}>
+    <NextIcon className="svg-icon" />
+  </button>
+  <div className="Time">
+    {calculateTime(currentTime)} / {calculateTime(duration)}
+  </div>
+  <div className="seek-control">
+    <input
+      type="range"
+      min="0"
+      max={duration || 0}
+      value={isSeeking ? undefined : currentTime}
+      onChange={handleSeekChange}
+      onMouseDown={handleSeekMouseDown}
+      onMouseUp={handleSeekMouseUp}
+      className="seek-slider"
+      step="1"
+    />
+  </div>
+  <div className="volume-control">
+    <input
+      type="range"
+      min="0"
+      max="1"
+      step="0.01"
+      value={volume}
+      onChange={handleVolumeChange}
+      className="volume-slider"
+    />
+  </div>
+</div>
+
   );
 };
 
