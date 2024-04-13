bc1e91098ca53250f286391f5f860cbb361b9513
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-12 23:15:43 -0700

    next function refactor

--- client/src/components/audioPlayer/AudioPlayer.jsx
+++ client/src/components/audioPlayer/AudioPlayer.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useCallback } from "react";
 import { ReactComponent as PlayIcon } from '../../images/Icons/play-blue.svg';
-import { ReactComponent as PauseIcon } from '../../images/Icons/Pause.svg'; // Note the capital 'P'
+import { ReactComponent as PauseIcon } from '../../images/Icons/Pause.svg';
 import { ReactComponent as NextIcon } from '../../images/Icons/next.svg';
 import { ReactComponent as PreviousIcon } from '../../images/Icons/back.svg';
 
@@ -8,7 +8,7 @@ import './AudioPlayer.css';
 
 const AudioPlayer = (props) => {
   const {
-    queue,
+    queue = [],
     currentSongIndex,
     setCurrentSongIndex,
     currentSong,
@@ -30,23 +30,20 @@ const AudioPlayer = (props) => {
     repeat
   } = props;
 
-  const playNextSong = () => {
-    if (repeat) {
-      audioPlayer.current.currentTime = 0;
-      audioPlayer.current.play();
-    } else {
-      let nextIndex = (currentSongIndex + 1) % queue.length;
-      setCurrentSongIndex(nextIndex);
-    }
-  };
+  const playNextSong = useCallback(() => {
+    let nextIndex = (currentSongIndex + 1) % queue.length;
+    console.log("Current index before update:", currentSongIndex);
+    console.log("Next index:", nextIndex);
+    setCurrentSongIndex(nextIndex);
+  }, [currentSongIndex, queue.length, setCurrentSongIndex]);
 
-  const playPreviousSong = () => {
+  const playPreviousSong = useCallback(() => {
     let prevIndex = (currentSongIndex - 1 + queue.length) % queue.length;
     setCurrentSongIndex(prevIndex);
-  };
+  }, [currentSongIndex, queue.length, setCurrentSongIndex]);
 
-  // Automatically play the current song when it changes
   useEffect(() => {
+    console.log(`Current song index is now ${currentSongIndex}, URL: ${queue[currentSongIndex]?.audio_url}`); // Debug log to see current song
     if (currentSong && audioPlayer.current) {
       audioPlayer.current.play().catch((error) => {
         console.error("Error attempting to play:", error);
@@ -55,11 +52,10 @@ const AudioPlayer = (props) => {
     }
   }, [currentSong, audioPlayer, setIsPlaying]);
 
-  // Handle what happens when audio ends
   useEffect(() => {
     const player = audioPlayer.current;
     const handleSongEnd = () => {
-      console.log("Audio playback ended");
+      console.log("Audio playback ended, moving to next song"); // Debug log
       playNextSong();
     };
 
@@ -71,55 +67,52 @@ const AudioPlayer = (props) => {
 
   return (
     <div className="player">
-  <audio
-    ref={audioPlayer}
-    src={currentSong ? currentSong.audio_url : ''}
-    onLoadedData={handleLoadedData}
-    onTimeUpdate={handleTimeUpdate}
-  ></audio>
-  <div className="audio-file-name">
-    {currentSong ? currentSong.name : "No song loaded"}
-  </div>
-  <button className="Back" onClick={playPreviousSong}>
-    <PreviousIcon className="svg-icon" />
-  </button>
-  <button className="Play" onClick={togglePlayPause}>
-    {isPlaying ? <PauseIcon className="svg-icon" />  : <PlayIcon className="svg-icon" />  
-
-}
-  </button>
-  <button className="Next" onClick={playNextSong}>
-    <NextIcon className="svg-icon" />
-  </button>
-  <div className="Time">
-    {calculateTime(currentTime)} / {calculateTime(duration)}
-  </div>
-  <div className="seek-control">
-    <input
-      type="range"
-      min="0"
-      max={duration || 0}
-      value={isSeeking ? undefined : currentTime}
-      onChange={handleSeekChange}
-      onMouseDown={handleSeekMouseDown}
-      onMouseUp={handleSeekMouseUp}
-      className="seek-slider"
-      step="1"
-    />
-  </div>
-  <div className="volume-control">
-    <input
-      type="range"
-      min="0"
-      max="1"
-      step="0.01"
-      value={volume}
-      onChange={handleVolumeChange}
-      className="volume-slider"
-    />
-  </div>
-</div>
-
+      <audio
+        ref={audioPlayer}
+        src={currentSong ? currentSong.audio_url : ''}
+        onLoadedData={handleLoadedData}
+        onTimeUpdate={handleTimeUpdate}
+      ></audio>
+      <div className="audio-file-name">
+        {currentSong ? currentSong.name : "No song loaded"}
+      </div>
+      <button className="Back" onClick={playPreviousSong}>
+        <PreviousIcon className="svg-icon" />
+      </button>
+      <button className="Play" onClick={togglePlayPause}>
+        {isPlaying ? <PauseIcon className="svg-icon" /> : <PlayIcon className="svg-icon" />}
+      </button>
+      <button className="Next" onClick={playNextSong}>
+        <NextIcon className="svg-icon" />
+      </button>
+      <div className="Time">
+        {calculateTime(currentTime)} / {calculateTime(duration)}
+      </div>
+      <div className="seek-control">
+        <input
+          type="range"
+          min="0"
+          max={duration || 0}
+          value={isSeeking ? undefined : currentTime}
+          onChange={handleSeekChange}
+          onMouseDown={handleSeekMouseDown}
+          onMouseUp={handleSeekMouseUp}
+          className="seek-slider"
+          step="1"
+        />
+      </div>
+      <div className="volume-control">
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={volume}
+          onChange={handleVolumeChange}
+          className="volume-slider"
+        />
+      </div>
+    </div>
   );
 };
 
