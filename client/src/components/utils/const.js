75574af47bfe02e6dc6e8f0a606703673204d2b9
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-14 10:20:35 -0700

    // file and location comment

--- client/src/components/utils/const.js
+++ client/src/components/utils/const.js
@@ -1,5 +1,5 @@
-// const. js -->> handles api & variables
+// /untils/const. js -->> handles api & variables
 
 // src/utils/constants.js
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001';  
