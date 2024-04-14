455e3215490118cf7634fcfa58d622545c57e4b3
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-13 23:15:44 -0700

    rename

--- server/routes/playerRoute.js
+++
@@ -1,9 +0,0 @@
-// playerRoutes.js
-
-const express = require('express');
-const router = express.Router();
-
-// Here you will later add route handlers, like:
-// router.get('/', (req, res) => res.send('User route placeholder'));
-
-module.exports = router;
