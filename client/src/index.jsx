fe71d7219daee19e8b9dabbac2d67fd2ec30d7c7
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-14 11:22:21 -0700

    update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,8 @@
-// index.js
+//index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppWrapper from './AppWrapper';
+import AppWrapper from './AppWrapper'; // Ensures all contexts and routers are included
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -14,7 +15,5 @@ root.render(
 
 console.log('ReactDOM rendered');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Optional: Performance logging
+reportWebVitals(console.log);
