cf47b603e3c184c0db2e35f2a8c2b260f48ff5eb
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-14 23:17:44 -0700

    updating this for the index to start at 1 in the app to  match the database indexID

--- client/src/components/Footer/Queue.jsx
+++ client/src/components/Footer/Queue.jsx
@@ -6,11 +6,18 @@ const Queue = () => {
     const [isExpanded, setIsExpanded] = useState(false);
     const { queue, currentSongIndex, setCurrentSongIndex } = useContext(CentralQueueContext);
 
+    // Log the queue's state each time the component renders
+    console.log("Queue loaded with current song index:", currentSongIndex);
+
+    // Toggle the visibility of the queue
     const toggleQueue = () => {
         setIsExpanded(!isExpanded);
+        console.log("Toggled queue visibility to:", !isExpanded);
     };
 
+    // Handle clicking on a song in the queue
     const handleSongClick = (index) => {
+        console.log("Attempting to set current song to index:", index); // Log which index is being attempted to set
         setCurrentSongIndex(index);
     };
 
@@ -18,15 +25,12 @@ const Queue = () => {
         <div className={`Queue ${isExpanded ? 'expanded' : ''}`} onClick={toggleQueue}>
             <div className="QueueTitle">Queue</div>
             {isExpanded && (
-                <div className="QueueItems">
+                <div className="QueueItems" onClick={e => e.stopPropagation()}>
                     {queue.length > 0 ? (
                         queue.map((song, index) => (
-                            <div key={index}
-                                 className={`QueueItem ${index === currentSongIndex ? 'highlight' : ''}`}
-                                 onClick={(e) => {
-                                     e.stopPropagation(); // Stop the queue from toggling
-                                     handleSongClick(index);
-                                 }}>
+                            <div key={song.globalIndex} // Using globalIndex as the key
+                                 className={`QueueItem ${song.globalIndex === currentSongIndex ? 'highlight' : ''}`}
+                                 onClick={() => handleSongClick(song.globalIndex)}>
                                 {song.name}
                             </div>
                         ))
