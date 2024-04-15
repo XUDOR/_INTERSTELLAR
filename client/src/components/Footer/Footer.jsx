0ceeb76ebdd0405d8c445f79eed04e2f338ea1ac
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-15 12:48:41 -0700

    removal of nav & admin buttons -> pages removed for MVP

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import Player from './Player';
 import Queue from './Queue';
 import Shop from './Shop';
-import Nav from './Nav';
-import Admin from './Admin';
+
 import User from './User';
 import Downloads from './Downloads';
 import './Footer.css';
@@ -12,10 +11,8 @@ const Footer = () => {
   return (
     <div className="AppContainer">
       <footer className="Footer">
-        <Admin />
         <Downloads />
         <User />
-        <Nav />
         <Player />  
         <Shop />
       </footer>
