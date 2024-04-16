08135fc8fedae31b4260dd25ed9815d360422dd4
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-15 21:15:24 -0700

    adding mediacontroller to appwrapper

--- client/src/AppWrapper.jsx
+++ client/src/AppWrapper.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { MusicDataProvider } from './Contexts/MusicDataContext';
-import { CentralQueueProvider } from './Contexts/CentralQueueContext'; // Ensure it's imported correctly
+import { CentralQueueProvider } from './Contexts/CentralQueueContext';
+import { MediaControlProvider } from './Contexts/MediaControlContext'; // Import the Media Control Context
 
 const AppWrapper = () => {
   return (
     <Router>
       <MusicDataProvider> {/* First level context for music data */}
         <CentralQueueProvider> {/* Second level context specifically for queue management */}
-          <App />
+          <MediaControlProvider> {/* Media Control context to manage playback across the app */}
+            <App />
+          </MediaControlProvider>
         </CentralQueueProvider>
       </MusicDataProvider>
     </Router>
