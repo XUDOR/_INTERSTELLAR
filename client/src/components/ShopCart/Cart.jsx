ba0544af7b63888415053d368aa54d669148b100
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-16 00:01:46 -0700

    basic cart

---
+++ client/src/components/ShopCart/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CartItem from './CartItem';
+import Total from './Total';
+
+const Cart = ({ cartItems, onRemoveFromCart }) => (
+  <div className="cart">
+    <h2 className="cart-title">CART</h2>
+    {cartItems.map((item) => (
+      <CartItem key={item.id} item={item} onRemoveFromCart={onRemoveFromCart} />
+    ))}
+    <Total cartItems={cartItems} />
+  </div>
+);
+
+export default Cart;
