3ad5ba655091bc0a5b7bf1ec6fd1696cf8512941
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-16 17:49:47 -0700

    axios introduced

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppWrapper from './AppWrapper'; // Ensures all contexts and routers are included
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001';  
+axios.defaults.baseURL=API_BASE_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,4 +19,4 @@ root.render(
 
 
 // Optional: Performance logging
-reportWebVitals(console.log);
+//reportWebVitals(console.log);
