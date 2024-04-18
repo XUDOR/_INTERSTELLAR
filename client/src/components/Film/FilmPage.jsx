16cbd168eb9684f161520d4a89dee763f5634d95
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-18 02:40:13 -0700

    film page -style

--- client/src/components/Film/FilmPage.jsx
+++ client/src/components/Film/FilmPage.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import './FilmPage.css'; // Make sure the path matches your file structure
+import './FilmPage.css'; // Ensure this path matches your file structure
 
 const FilmPage = () => {
   return (
     <div className="FilmContainers">
       <div className="FilmContainer">
         <iframe
-          src="https://player.vimeo.com/video/935038582"
-          frameborder="0"
+          src="https://player.vimeo.com/video/935038582?badge=0&autopause=0&player_id=0&app_id=58479"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-          title="Film Title 1"
+          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+          title="FILMS OF NATURE | Glass City of Us | Roderick Shoolbraid"
           loading="lazy"
         ></iframe>
       </div>
       <div className="FilmContainer">
         <iframe
-          src="https://player.vimeo.com/video/935116508"
-          frameborder="0"
+          src="https://player.vimeo.com/video/935116508?badge=0&autopause=0&player_id=0&app_id=58479"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-          title="Film Title 2"
+          title="TIDAL | NATURA | Roderick Shoolbraid"
           loading="lazy"
         ></iframe>
       </div>
