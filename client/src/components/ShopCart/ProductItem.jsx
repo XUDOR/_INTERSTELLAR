143e99abb06ca6a5d66ea735ee67edfc58aef09f
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-25 16:48:15 -0700

    changes to code in beginning new refacto-start

--- client/src/components/ShopCart/ProductItem.jsx
+++ client/src/components/ShopCart/ProductItem.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-const ProductItem = ({ product, onAddToCart }) => (
-  <div className="product-item">
-    <div className="product-cover">
-      COVER
+const ProductItem = ({ product, onAddToCart }) => {
+  // Replace spaces with dashes and append '_thumb.png' to form the filename
+  const imageName = `${product.id}_${product.name.toUpperCase().replace(/ /g, '-')}_thumb.png`;
+
+  return (
+    <div className="product-item">
+      <div className="product-cover">
+        <img src={`../images/ART_THUMBNAILS-CART_DISPLAY/${imageName}`} alt={product.name} />
+      </div>
+      <div className="product-details">
+        <h3 className="product-name">{product.name}</h3>
+        <p className="product-price">${product.price.toFixed(2)}</p>
+        <button className="add-product" onClick={() => onAddToCart(product)}>+</button>
+      </div>
     </div>
-    <div className="product-details">
-      <h3 className="product-name">{product.name}</h3>
-      <p className="product-price">${product.price.toFixed(2)}</p>
-      <button className="add-product" onClick={() => onAddToCart(product)}>+</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ProductItem;
