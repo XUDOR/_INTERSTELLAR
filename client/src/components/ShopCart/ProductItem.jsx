940d80628a24aa903cd21c16f4f71839431ea8f5
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-26 09:31:52 -0700

    classname for css organization

--- client/src/components/ShopCart/ProductItem.jsx
+++ client/src/components/ShopCart/ProductItem.jsx
@@ -4,6 +4,7 @@ const ProductItem = ({ product, onAddToCart }) => {
       <div className="product-cover">
         <img src={product.image} alt={product.name} />
       </div>
+      <div className="product-info-container">
       <div className="product-details">
         <h3 className="product-name">{product.name}</h3>
         <div className="product-catalogue">{product.catalogueID}</div>
@@ -11,6 +12,8 @@ const ProductItem = ({ product, onAddToCart }) => {
       <div className="price-addCart">
       <p className="product-price">${product.price.toFixed(2)}</p>
       <button className="add-to-cart" onClick={() => onAddToCart(product)}>+</button>
+      </div>
+
       </div>
     </div>
   );
