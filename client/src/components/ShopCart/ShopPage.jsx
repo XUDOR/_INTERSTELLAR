9f0c796dc8274df92b03c019a432ea77d5cd28a8
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-26 12:01:47 -0700

    style refactor v4

--- client/src/components/ShopCart/ShopPage.jsx
+++ client/src/components/ShopCart/ShopPage.jsx
@@ -9,7 +9,7 @@ const products = [
     { id: 2, name: 'Objects & Particles', price: 7.00, catalogueID: '#00040102', image: '/images/ART_THUMBNAILS-CART_DISPLAY/2_OBJECTS-PARTICLES_thumb.png' },
     { id: 3, name: 'Glass City of Us', price: 7.00, catalogueID: '#00040103', image: '/images/ART_THUMBNAILS-CART_DISPLAY/3_GLASS-CITY-OF-US_thumb.png' },
     { id: 4, name: 'New Domes of Earth', price: 7.00, catalogueID: '#00040104', image: '/images/ART_THUMBNAILS-CART_DISPLAY/4_NEW-DOMES_thumb.png' },
-    { id: 5, name: 'Natura', price: 10.00, catalogueID: '#01040105 — digital', image: '/images/ART_THUMBNAILS-CART_DISPLAY/5_NATURA_thumb.png' },
+    { id: 5, name: 'Natura', price: 10.00, catalogueID: '#01040105 /digital', image: '/images/ART_THUMBNAILS-CART_DISPLAY/5_NATURA_thumb.png' },
     { id: 6, name: 'Outer Corners', price: 8.00, catalogueID: '#01040106', image: '/images/ART_THUMBNAILS-CART_DISPLAY/6_OUTER-CORNERS_thumb.png' },
     { id: 7, name: 'Nonagon', price: 10.00, catalogueID: '#01040107', image: '/images/ART_THUMBNAILS-CART_DISPLAY/7_NONAGON_thumb.png' },
     { id: 8, name: 'Ambient Garden One', price: 15.00, catalogueID: '#01040108', image: '/images/ART_THUMBNAILS-CART_DISPLAY/8_AMBIENTGARDENONE_thumb.png' },
@@ -24,7 +24,7 @@ const products = [
     { id: 17, name: 'Sakura', price: 8.00, catalogueID: '#01040117', image: '/images/ART_THUMBNAILS-CART_DISPLAY/17_SAKURA_thumb.png' },
     { id: 18, name: 'Poem for a Homeworld', price: 16.00, catalogueID: '#01040118', image: '/images/ART_THUMBNAILS-CART_DISPLAY/18_POEM-HOMEWORLD_thumb.png' },
     { id: 19, name: 'Rhombus', price: 11.00, catalogueID: '#01040119', image: '/images/ART_THUMBNAILS-CART_DISPLAY/19_RHOMBUS_thumb.png' },
-    { id: 20, name: 'Natura - Cassette', price: 12.00, catalogueID: '#01020105 — cassette', image: '/images/ART_THUMBNAILS-CART_DISPLAY/5_NATURA_thumb.png' },
+    { id: 20, name: 'Natura - Cassette', price: 12.00, catalogueID: '#01020105 /cassette', image: '/images/ART_THUMBNAILS-CART_DISPLAY/5_NATURA_thumb.png' },
     { id: 21, name: 'Discography', price: 100.00, catalogueID: '#010401A', image: '/images/ART_THUMBNAILS-CART_DISPLAY/A_Discography_thumb.png' }
 ];
 
