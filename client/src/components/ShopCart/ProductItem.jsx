f56c75de9dc84a44a737d5fee0747876f7638f02
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-26 00:27:13 -0700

    rename | refactor

--- client/src/components/ShopCart/ProductItem.jsx
+++ client/src/components/ShopCart/ProductItem.jsx
@@ -2,15 +2,14 @@ const ProductItem = ({ product, onAddToCart }) => {
   return (
     <div className="product-item">
       <div className="product-cover">
-        {/* Make sure the path is correct. The product.image should be a path relative to the public directory. */}
-        <img src={`/images/${product.image}`} alt={product.name} />
+        <img src={product.image} alt={product.name} />
       </div>
       <div className="product-details">
         <h3 className="product-name">{product.name}</h3>
         <div className="product-catalogue">{product.catalogueID}</div>
         <p className="product-price">${product.price.toFixed(2)}</p>
-        <button className="add-to-cart" onClick={() => onAddToCart(product)}>+</button>
       </div>
+      <button className="add-to-cart" onClick={() => onAddToCart(product)}>+</button>
     </div>
   );
 };
