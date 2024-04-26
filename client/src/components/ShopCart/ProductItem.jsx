dc11aed4bee3a54439586a1df944ac9c289bc276
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-25 23:12:30 -0700

    Refactor for hardcoded product object

--- client/src/components/ShopCart/ProductItem.jsx
+++ client/src/components/ShopCart/ProductItem.jsx
@@ -1,13 +1,8 @@
-import React from "react";
-
 const ProductItem = ({ product, onAddToCart }) => {
-  // Replace spaces with dashes and append '_thumb.png' to form the filename
-  const imageName = `${product.id}_${product.name.toUpperCase().replace(/ /g, '-')}_thumb.png`;
-
   return (
     <div className="product-item">
       <div className="product-cover">
-        <img src={`../images/ART_THUMBNAILS-CART_DISPLAY/${imageName}`} alt={product.name} />
+        <img src={`../images/ART_THUMBNAILS-CART_DISPLAY/${product.image}`} alt={product.name} />
       </div>
       <div className="product-details">
         <h3 className="product-name">{product.name}</h3>
