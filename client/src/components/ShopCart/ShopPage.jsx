5f14e9e6666f2ad19877457ab82d2831083ca8ef
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-25 23:39:26 -0700

    refactor to add product  cat# and other refactors

--- client/src/components/ShopCart/ShopPage.jsx
+++ client/src/components/ShopCart/ShopPage.jsx
@@ -5,165 +5,28 @@ import './ShopPage.css';
 
 const ShopPage = () => {
   const [products, setProducts] = useState([
-    { id: 1, 
-      name: 'Charlotta', 
-      price: 9.00, 
-      catalogueID: '#00040101', 
-      image: 'ART_THUMBNAILS-CART_DISPLAY/1_CHARLOTTA_thumb.png' },
-
-    { id: 2, 
-      name: 'Objects & Particles', 
-      price: 7.00, 
-      catalogueID: '#00040102',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/2_OBJECTS-PARTICLES_thumb.png' },
-  
-    { id: 3, 
-      name: 'Glass City of Us', 
-      price: 7.00, 
-      catalogueID: '#00040103',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/3_GLASS-CITY-OF-US_thumb.png'
-     },
-
-    { id: 4, 
-      name: 'New Domes of Earth', 
-      price: 7.00, 
-      catalogueID: '#00040104', 
-      image: 'ART_THUMBNAILS-CART_DISPLAY/4_NEW-DOMES_thumb.png'
-    },
-
-    { id: 5, 
-      name: 'Natura', 
-      price: 10.00, 
-      catalogueID: '#01040105 — digital',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/5_NATURA_thumb.png'
-       },
-
-    { id: 6, 
-      name: 'Outer Corners', 
-      price: 8.00, 
-      catalogueID: '#01040106',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/6_OUTER-CORNERS_thumb.png',
-     },
-
-    { id: 7, 
-      name: 'Nonagon', 
-      price: 10.00, 
-      catalogueID: '#01040107',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/7_NONAGON_thumb.png',
-    
-    },
-
-    { id: 8, 
-      name: 'Ambient Garden One', 
-      price: 15.00, 
-      catalogueID: '#01040108',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/8_AMBIENTGARDENONE_thumb.png',
-    
-    },
-
-    { id: 9, 
-      name: 'Music for Seven Structures', 
-      price: 8.00, 
-      catalogueID: '#01040109',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/9_7STRUCTURES_thumb.png',
-    
-    },
-
-    { id: 10, 
-      name: 'Inner Moments of Light', 
-      price: 10.00, 
-      catalogueID: '#01040110',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/10_INNER-MOMENTS_thumb.png',
-    
-    },
-
-    { id: 11, 
-      name: 'Nocturnes & Reveries', 
-      price: 16.00, 
-      catalogueID: '#01040111',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/11_NOCTURNES-REVERIES_thumb.png',
-    
-    },
-
-    { id: 12, 
-      name: 'Prefabrication', 
-      price: 11.00, 
-      catalogueID: '#01040112',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/12_PREFABRICATION_thumb.png',
-    
-    },
-
-    { id: 13, 
-      name: 'Vagary', 
-      price: 9.00, 
-      catalogueID: '#01040113',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/13_VAGARY_thumb.png',
-    
-    },
-
-    { id: 14, 
-      name: 'Amsterdam Concreet', 
-      price: 10.00, 
-      catalogueID: '#01040114',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/14_AMSTERDAM-CONCREET_thumb.png',
-    
-    },
-
-    { id: 15, 
-      name: 'Watercolours for Friends', 
-      price: 8.00, 
-      catalogueID: '#01040115',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/15_WATERCOLOURS_thumb.png',
-    
-    },
-
-    { id: 16, 
-      name: 'Postcards from Old Sounds', 
-      price: 7.00, 
-      catalogueID: '#01040116',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/16_POSTACARDS-OLD-SOUNDS_thumb.png',
-    
-    },
-
-    { id: 17, 
-      name: 'Sakura', 
-      price: 8.00, 
-      catalogueID: '#01040117',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/17_SAKURA_thumb.png',
-    
-    },
-
-    { id: 18, 
-      name: 'Poem for a Homeworld', 
-      price: 16.00, 
-      catalogueID: '#01040118',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/18_POEM-HOMEWORLD_thumb.png',
-    
-    },
-
-    { id: 19, 
-      name: 'Rhombus', 
-      price: 11.00, 
-      catalogueID: '#01040119' ,
-      image: 'ART_THUMBNAILS-CART_DISPLAY/19_RHOMBUS_thumb.png',
-    },
-
-    { id: 20, 
-      name: 'Natura - Cassette', 
-      price: 12.00, 
-      catalogueID: '#01020105 — cassette',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/5_NATURA_thumb.png',
-    },
-    { id: 21, 
-      name: 'Discography', 
-      price: 100.00,
-      catalogueID: '#010401A',
-      image: 'ART_THUMBNAILS-CART_DISPLAY/A_Discography_thumb.png',
-    },
-    
-    // Continue adding the rest of your products
+    { id: 1, name: 'Charlotta', price: 9.00, catalogueID: '#00040101', image: '/images/ART_THUMBNAILS-CART_DISPLAY/1_CHARLOTTA_thumb.png' },
+    { id: 2, name: 'Objects & Particles', price: 7.00, catalogueID: '#00040102', image: '/images/ART_THUMBNAILS-CART_DISPLAY/2_OBJECTS-PARTICLES_thumb.png' },
+    { id: 3, name: 'Glass City of Us', price: 7.00, catalogueID: '#00040103', image: '/images/ART_THUMBNAILS-CART_DISPLAY/3_GLASS-CITY-OF-US_thumb.png' },
+    { id: 4, name: 'New Domes of Earth', price: 7.00, catalogueID: '#00040104', image: '/images/ART_THUMBNAILS-CART_DISPLAY/4_NEW-DOMES_thumb.png' },
+    { id: 5, name: 'Natura', price: 10.00, catalogueID: '#01040105 — digital', image: '/images/ART_THUMBNAILS-CART_DISPLAY/5_NATURA_thumb.png' },
+    { id: 6, name: 'Outer Corners', price: 8.00, catalogueID: '#01040106', image: '/images/ART_THUMBNAILS-CART_DISPLAY/6_OUTER-CORNERS_thumb.png' },
+    { id: 7, name: 'Nonagon', price: 10.00, catalogueID: '#01040107', image: '/images/ART_THUMBNAILS-CART_DISPLAY/7_NONAGON_thumb.png' },
+    { id: 8, name: 'Ambient Garden One', price: 15.00, catalogueID: '#01040108', image: '/images/ART_THUMBNAILS-CART_DISPLAY/8_AMBIENTGARDENONE_thumb.png' },
+    { id: 9, name: 'Music for Seven Structures', price: 8.00, catalogueID: '#01040109', image: '/images/ART_THUMBNAILS-CART_DISPLAY/9_7STRUCTURES_thumb.png' },
+    { id: 10, name: 'Inner Moments of Light', price: 10.00, catalogueID: '#01040110', image: '/images/ART_THUMBNAILS-CART_DISPLAY/10_INNER-MOMENTS_thumb.png' },
+    { id: 11, name: 'Nocturnes & Reveries', price: 16.00, catalogueID: '#01040111', image: '/images/ART_THUMBNAILS-CART_DISPLAY/11_NOCTURNES-REVERIES_thumb.png' },
+    { id: 12, name: 'Prefabrication', price: 11.00, catalogueID: '#01040112', image: '/images/ART_THUMBNAILS-CART_DISPLAY/12_PREFABRICATION_thumb.png' },
+    { id: 13, name: 'Vagary', price: 9.00, catalogueID: '#01040113', image: '/images/ART_THUMBNAILS-CART_DISPLAY/13_VAGARY_thumb.png' },
+    { id: 14, name: 'Amsterdam Concreet', price: 10.00, catalogueID: '#01040114', image: '/images/ART_THUMBNAILS-CART_DISPLAY/14_AMSTERDAM-CONCREET_thumb.png' },
+    { id: 15, name: 'Watercolours for Friends', price: 8.00, catalogueID: '#01040115', image: '/images/ART_THUMBNAILS-CART_DISPLAY/15_WATERCOLOURS_thumb.png' },
+    { id: 16, name: 'Postcards from Old Sounds', price: 7.00, catalogueID: '#01040116', image: '/images/ART_THUMBNAILS-CART_DISPLAY/16_POSTACARDS-OLD-SOUNDS_thumb.png' },
+    { id: 17, name: 'Sakura', price: 8.00, catalogueID: '#01040117', image: '/images/ART_THUMBNAILS-CART_DISPLAY/17_SAKURA_thumb.png' },
+    { id: 18, name: 'Poem for a Homeworld', price: 16.00, catalogueID: '#01040118', image: '/images/ART_THUMBNAILS-CART_DISPLAY/18_POEM-HOMEWORLD_thumb.png' },
+    { id: 19, name: 'Rhombus', price: 11.00, catalogueID: '#01040119', image: '/images/ART_THUMBNAILS-CART_DISPLAY/19_RHOMBUS_thumb.png' },
+    { id: 20, name: 'Natura - Cassette', price: 12.00, catalogueID: '#01020105 — cassette', image: '/images/ART_THUMBNAILS-CART_DISPLAY/5_NATURA_thumb.png' },
+    { id: 21, name: 'Discography', price: 100.00, catalogueID: '#010401A', image: '/images/ART_THUMBNAILS-CART_DISPLAY/A_Discography_thumb.png' }
   ]);
-  
 
   const [cartItems, setCartItems] = useState([]);
 
