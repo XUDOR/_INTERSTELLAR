320fd22ee929ebe281bbdb5c63207cea45b8855d
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-25 23:40:05 -0700

    changed "add-to cart" button

--- client/src/components/ShopCart/ProductItem.jsx
+++ client/src/components/ShopCart/ProductItem.jsx
@@ -2,12 +2,14 @@ const ProductItem = ({ product, onAddToCart }) => {
   return (
     <div className="product-item">
       <div className="product-cover">
-        <img src={`../images/ART_THUMBNAILS-CART_DISPLAY/${product.image}`} alt={product.name} />
+        {/* Make sure the path is correct. The product.image should be a path relative to the public directory. */}
+        <img src={`/images/${product.image}`} alt={product.name} />
       </div>
       <div className="product-details">
         <h3 className="product-name">{product.name}</h3>
+        <div className="product-catalogue">{product.catalogueID}</div>
         <p className="product-price">${product.price.toFixed(2)}</p>
-        <button className="add-product" onClick={() => onAddToCart(product)}>+</button>
+        <button className="add-to-cart" onClick={() => onAddToCart(product)}>+</button>
       </div>
     </div>
   );
