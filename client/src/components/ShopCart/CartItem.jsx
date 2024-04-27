f44d47f3820622059b7105f9a7b1f047ab36c769
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-26 17:07:14 -0700

    rename-refactor for non-grid untethering

--- client/src/components/ShopCart/CartItem.jsx
+++ client/src/components/ShopCart/CartItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const CartItem = ({ item, onRemoveFromCart }) => (
-  <div className="cart-item">
-    <span className="item-name">{item.name} x {item.quantity}</span>
-    <span className="item-price">${(item.price * item.quantity).toFixed(2)}</span>
-    <button className="remove-item" onClick={() => onRemoveFromCart(item.id)}>Remove</button>
+  <div className="cart-container">
+    <div className="cart-item">
+        <span className="item-name">{item.name} x {item.quantity}</span>
+        <span className="item-price">${(item.price * item.quantity).toFixed(2)}</span>
+    </div>
+    <button className="remove-item" onClick={() => onRemoveFromCart(item.id)}></button>
   </div>
 );
 
