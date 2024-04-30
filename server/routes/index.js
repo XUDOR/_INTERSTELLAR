3195d109bb25920cf166eca6d6509252e38a361b
Roderick Shoolbraid <rdxenv@gmail.com>
2024-04-29 19:21:29 -0700

    payment api

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
-
 const albumRoutes = require('./albumRoutes');
 const songRoutes = require('./songRoutes');
 const centralRoutes = require('./centralRoutes'); 
+const paymentRoutes = require('./paymentRoutes'); 
 const stripeWebhook = require('../webhooks/stripeWebhook');
 
 //const adminRoutes = require('./adminRoutes');
@@ -15,7 +15,7 @@ router.use('/albums', albumRoutes);
 router.use('/songs', songRoutes);
 router.use('/central', centralRoutes);
 router.use('/webhooks/stripe', stripeWebhook);
-
+router.use('/api', paymentRoutes); 
 //router.use('/admin', adminRoutes);
 
 
