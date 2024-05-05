55b267c0674d18c4590f44f41f53196e90beea94
Roderick Shoolbraid <rdxenv@gmail.com>
2024-05-05 08:24:28 -0700

    page update (merge-copy from other branch)

--- client/src/components/Contact/ContactPage.jsx
+++ client/src/components/Contact/ContactPage.jsx
@@ -1,6 +1,59 @@
+// ContactPage.jsx
 import React from 'react';
 import './ContactPage.css'; 
 
+const ContactPage = () => {
+  return (
+    <>
+      <div className="ContactContent"> {/* Add this wrapper */}
+        <div className='ContactTitle'>_Contact</div>
+        <div className="MasterContainer">
+          <div className="Container1">
+            <div className="Picture">
+              {/* Your picture goes here */}
+            </div>
+            <div className="Info1">
+              {/* Info 1 content */}
+              <p>Some text goes here</p>
+            </div>
+          </div>
+          <div className="Container2">
+            {/* Info 2 content */}
+            <p>Some more text goes here</p>
+          </div>
+          <div className="Container3">
+            <form action="mailto:interstellarpackages@gmail.com" method="post" encType="text/plain">
+              <div className="FormField">
+                
+                <label htmlFor="name">Name:</label>
+                <input classname="nameInput"type="text" id="name" name="name" />
+              </div>
+              <div className="FormField">
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" />
+              </div>
+              <div className="FormField">
+                <label htmlFor="date">Date:</label>
+                <input type="date" id="date" name="date" />
+              </div>
+              <div className="FormField">
+                <label className="formContent"htmlFor="content">Content:</label>
+                <textarea id="content" name="content"></textarea>
+              </div>
+              <div className="FormField">
+                <button className="submit"type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactPage;
+
+/*
 const ContactPage = () => {
   return (
     <>
@@ -20,3 +73,5 @@ const ContactPage = () => {
 };
 
 export default ContactPage;
+
+*/
---
+++ client/src/components/Contact/ContactPagex.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ContactPage.css'; 
+
+const ContactPage = () => {
+  return (
+    <>
+      <div className="ContactContent"> 
+        
+      </div>
+      <div className="ContactDetails">
+        <div className="Column1">Roderick Shoolbraid</div>
+        <div className="Column2">
+          778 922 8992 <br />
+          rdxenv@gmail.com <br />
+          interstellarpackages@gmail.com
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactPage;
