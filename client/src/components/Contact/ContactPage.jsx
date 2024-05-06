e7269a7a5a3433b0b4670710507e60921f7eb970
Roderick Shoolbraid <rdxenv@gmail.com>
2024-05-05 18:16:56 -0700

    70% done...

--- client/src/components/Contact/ContactPage.jsx
+++ client/src/components/Contact/ContactPage.jsx
@@ -1,77 +1,50 @@
-// ContactPage.jsx
 import React from 'react';
-import './ContactPage.css'; 
+import './ContactPage.css';
 
 const ContactPage = () => {
   return (
-    <>
-      <div className="ContactContent"> {/* Add this wrapper */}
-        <div className='ContactTitle'>_Contact</div>
-        <div className="MasterContainer">
-          <div className="Container1">
-            <div className="Picture">
-              {/* Your picture goes here */}
-            </div>
-            <div className="Info1">
-              {/* Info 1 content */}
-              <p>Some text goes here</p>
-            </div>
+    <div className="ContactContent">
+      <div className="MasterContainer">
+        <div className="Container1">
+          <div className="Picture">
+            {/* Include the image directly in JSX */}
+            <img src="/pictures/RODERICK_SHOOLBRAID-PROMO1_2022sm.png" alt="Roderick Shoolbraid Promo" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
           </div>
-          <div className="Container2">
-            {/* Info 2 content */}
-            <p>Some more text goes here</p>
-          </div>
-          <div className="Container3">
-            <form action="mailto:interstellarpackages@gmail.com" method="post" encType="text/plain">
-              <div className="FormField">
-                
-                <label htmlFor="name">Name:</label>
-                <input classname="nameInput"type="text" id="name" name="name" />
-              </div>
-              <div className="FormField">
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
-              </div>
-              <div className="FormField">
-                <label htmlFor="date">Date:</label>
-                <input type="date" id="date" name="date" />
-              </div>
-              <div className="FormField">
-                <label className="formContent"htmlFor="content">Content:</label>
-                <textarea id="content" name="content"></textarea>
-              </div>
-              <div className="FormField">
-                <button className="submit"type="submit">Submit</button>
-              </div>
-            </form>
+          <div className="Info1">
+            <p>Contact Details</p>
           </div>
         </div>
+        <div className="Container2">
+          <div className="Info2">
+            <p>Bio / EPK</p>
+          </div>
+        </div>
+        <div className="Container3">
+          <form action="mailto:interstellarpackages@gmail.com" method="post" encType="text/plain">
+            <div className="FormField-name">
+              <label htmlFor="name">Name:</label>
+              <input className="nameInput" type="text" id="name" name="name" />
+            </div>
+            <div className="FormField-email">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" />
+            </div>
+            <div className="FormField-date">
+              <label htmlFor="date">Date:</label>
+              <input type="date" id="date" name="date" />
+            </div>
+            <div className="FormField-formContent">
+              <label className="formContent" htmlFor="content">Content:</label>
+              <textarea id="content" name="content"></textarea>
+            </div>
+            <div className="FormField-submit">
+              <button className="submit" type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
 export default ContactPage;
-
-/*
-const ContactPage = () => {
-  return (
-    <>
-      <div className="ContactContent"> 
-        
-      </div>
-      <div className="ContactDetails">
-        <div className="Column1">Roderick Shoolbraid</div>
-        <div className="Column2">
-          778 922 8992 <br />
-          rdxenv@gmail.com <br />
-          interstellarpackages@gmail.com
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default ContactPage;
-
-*/
