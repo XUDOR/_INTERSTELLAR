73324913e58a1170b4eab7d186522734f8f14851
Roderick Shoolbraid <rdxenv@gmail.com>
2024-05-08 16:51:59 -0700

    imported route to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,18 @@
-//index.js
-
 const express = require('express');
 const router = express.Router();
 const albumRoutes = require('./albumRoutes');
 const songRoutes = require('./songRoutes');
-const centralRoutes = require('./centralRoutes'); 
-const paymentRoutes = require('./paymentRoutes'); 
+const centralRoutes = require('./centralRoutes');
+const paymentRoutes = require('./paymentRoutes');
 const stripeWebhook = require('../webhooks/stripeWebhook');
+const contactRoutes = require('./contactRoutes');  // Import contact routes
 
-//const adminRoutes = require('./adminRoutes');
-
-
+// Setup route middlewares
 router.use('/albums', albumRoutes);
 router.use('/songs', songRoutes);
 router.use('/central', centralRoutes);
 router.use('/webhooks/stripe', stripeWebhook);
 router.use('/payment', paymentRoutes);
- 
-//router.use('/admin', adminRoutes);
-
+router.use('/contact', contactRoutes);  // Use contact routes
 
 module.exports = router;
