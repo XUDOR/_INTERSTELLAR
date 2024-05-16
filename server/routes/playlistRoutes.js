fdb86e2dd67c5c63504b966bafd777c581a066d3
Roderick Shoolbraid <rdxenv@gmail.com>
2024-05-16 15:41:33 -0700

    update - route  for playlist to be seen

--- server/routes/playlistRoutes.js
+++ server/routes/playlistRoutes.js
@@ -1,8 +1,6 @@
-//playlistRoutes.js
-
 const express = require('express');
 const router = express.Router();
-const { savePlaylist } = require('../services/playlistService');
+const { savePlaylist, getPlaylists } = require('../services/playlistService');
 
 router.post('/', async (req, res) => {
     const { name, songs } = req.body;
@@ -12,6 +10,16 @@ router.post('/', async (req, res) => {
         res.status(201).json({ playlistId });
     } catch (err) {
         console.error('Error saving playlist:', err);
+        res.status(500).send('Server error');
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const playlists = await getPlaylists();
+        res.json(playlists);
+    } catch (err) {
+        console.error('Error fetching playlists:', err);
         res.status(500).send('Server error');
     }
 });
