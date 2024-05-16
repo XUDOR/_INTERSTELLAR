bd89ebe74d3848338a47e97a4e7ed35cf0d3c033
Roderick Shoolbraid <rdxenv@gmail.com>
2024-05-16 15:41:47 -0700

    service update for playlist

--- server/services/playlistService.js
+++ server/services/playlistService.js
@@ -2,10 +2,12 @@ const pool = require('../db');
 
 const savePlaylist = async (name, songs) => {
     const client = await pool.connect();
+    const public_user_id = 1; // Default public user ID
+
     try {
         await client.query('BEGIN');
         const insertPlaylistText = 'INSERT INTO playlists (user_id, name) VALUES ($1, $2) RETURNING id';
-        const res = await client.query(insertPlaylistText, [/* User ID here */, name]);
+        const res = await client.query(insertPlaylistText, [public_user_id, name]);
         const playlistId = res.rows[0].id;
 
         const insertSongsText = 'INSERT INTO playlist_songs (playlist_id, song_id) VALUES ($1, $2)';
@@ -23,6 +25,23 @@ const savePlaylist = async (name, songs) => {
     }
 };
 
+const getPlaylists = async () => {
+    const client = await pool.connect();
+    try {
+        const res = await client.query(`
+            SELECT p.id, p.name, json_agg(s.*) as songs
+            FROM playlists p
+            JOIN playlist_songs ps ON p.id = ps.playlist_id
+            JOIN songs s ON ps.song_id = s.id
+            GROUP BY p.id
+        `);
+        return res.rows;
+    } finally {
+        client.release();
+    }
+};
+
 module.exports = {
-    savePlaylist
+    savePlaylist,
+    getPlaylists
 };
