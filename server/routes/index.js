6451e399c5ac462fc094faf2f1103eba68845be4
Roderick Shoolbraid <rdxenv@gmail.com>
2024-05-16 09:14:25 -0700

    playlist to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,21 @@
+//index.js
+
 const express = require('express');
 const router = express.Router();
 const albumRoutes = require('./albumRoutes');
 const songRoutes = require('./songRoutes');
 const centralRoutes = require('./centralRoutes');
+const playlistRoutes = require('./playlistRoutes');  // Import playlist routes
 const paymentRoutes = require('./paymentRoutes');
 const stripeWebhook = require('../webhooks/stripeWebhook');
-const contactRoutes = require('./contactRoutes');  // Import contact routes
+const contactRoutes = require('./contactRoutes'); 
 
-// Setup route middlewares
 router.use('/albums', albumRoutes);
 router.use('/songs', songRoutes);
 router.use('/central', centralRoutes);
+router.use('/playlists', playlistRoutes);  // Use playlist routes
 router.use('/webhooks/stripe', stripeWebhook);
 router.use('/payment', paymentRoutes);
-router.use('/contact', contactRoutes);  // Use contact routes
+router.use('/contact', contactRoutes);
 
 module.exports = router;
